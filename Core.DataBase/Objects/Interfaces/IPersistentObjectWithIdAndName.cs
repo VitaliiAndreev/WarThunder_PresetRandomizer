8d3d0114c0250a7b04806a0c3b7c1e535f03ed6b
Vitalii Andreev <coverthelirush@gmail.com>
2019-02-23 14:42:41 +0200

    feature: add (I)PersistentObjectWithIdAndName

---
+++ Core.DataBase/Objects/Interfaces/IPersistentObjectWithIdAndName.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DataBase.Objects.Interfaces
+{
+    /// <summary> A persistent (stored in a database) object that has an ID and a name. </summary>
+    public interface IPersistentObjectWithIdAndName : IPersistentObjectWithId
+    {
+        /// <summary> The object's name. </summary>
+        string Name { get; }
+    }
+}
---
+++ Core.DataBase/Objects/PersistentObjectWithIdAndName.cs
@@ -0,0 +1,57 @@
+﻿using Core.DataBase.Helpers.Interfaces;
+using Core.DataBase.Objects.Interfaces;
+using Core.Extensions;
+
+namespace Core.DataBase.Objects
+{
+    /// <summary> A persistent (stored in a database) object that has an ID and a name. </summary>
+    public class PersistentObjectWithIdAndName : PersistentObjectWithId, IPersistentObjectWithIdAndName
+    {
+        #region Fields
+
+        /// <summary>
+        /// The object's name.
+        /// This field is used by the <see cref="Name"/> property.
+        /// </summary>
+        protected string _name;
+
+        #endregion Fields
+        #region Properties
+
+        /// <summary> The object's name. </summary>
+        public virtual string Name
+        {
+            get { return _name; }
+            protected set { _name = value; }
+        }
+
+        #endregion Properties
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new transient object that can be persisted later.
+        /// This constructor is used to maintain inheritance of class composition required for NHibernate mapping.
+        /// </summary>
+        protected PersistentObjectWithIdAndName()
+        {
+        }
+
+        /// <summary> Creates a new transient object that can be persisted later. </summary>
+        /// <param name="dataRepository"> A data repository to persist the object with. </param>
+        /// <param name="id"> The object's ID. </param>
+        /// <param name="name"> The object's name. </param>
+        protected PersistentObjectWithIdAndName(IDataRepository dataRepository, string id, string name)
+            : base(dataRepository, id)
+        {
+            _name = name;
+
+            LogCreation();
+        }
+
+        #endregion Constructors
+
+        /// <summary> Returns a string that represents the instance. </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{base.ToString().SkipLast(1)}, Name: \"{_name}\")";
+    }
+}
