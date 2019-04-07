b5389aa105f602dea7f7aba92d35e4342754ef03
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-07 17:34:21 +0200

    test: write UTs for FileReader.Read()

---
+++ Core.Tests/Helpers/FileReaderTests.cs
@@ -0,0 +1,79 @@
+﻿using Core.DataBase.Tests;
+using Core.Helpers;
+using Core.Helpers.Interfaces;
+using Core.Helpers.Logger.Enumerations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Core.Tests.Helpers
+{
+    /// <summary> See <see cref="FileReader"/>. </summary>
+    [TestClass]
+    public class FileReaderTests
+    {
+        private IFileManager _fileManager;
+        private IFileReader _fileReader;
+        private string _rootDirectory;
+
+        #region Internal Methods
+
+        [TestInitialize]
+        public void CreateFileManager()
+        {
+            _fileManager = new FileManager(Presets.Logger);
+            _fileReader = new FileReader(Presets.Logger);
+            _rootDirectory = $"{Directory.GetCurrentDirectory()}\\TestFiles";
+
+            if (!Directory.Exists(_rootDirectory))
+                Directory.CreateDirectory(_rootDirectory);
+            else
+                _fileManager.EmptyDirectory(_rootDirectory);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Presets.Logger.LogInfo(ECoreLogCategory.UnitTests, ECoreLogMessage.CleanUpAfterUnitTestStartsHere);
+            _fileManager.DeleteDirectory(_rootDirectory);
+        }
+
+        #endregion Internal Methods
+        #region Tests: Read()
+
+        [TestMethod]
+        public void Read_NoFile_ShouldThrowFileNotFoundException()
+        {
+            // arrange
+            var filePath = $"{_rootDirectory}\\iDoNotExist.sad";
+            File.Exists(filePath).Should().BeFalse();
+
+            // act
+            Action readFile = () => _fileReader.Read(filePath);
+
+            // assert
+            readFile.Should().Throw<FileNotFoundException>();
+        }
+
+        [TestMethod]
+        public void Read_FileExists_ShouldRead()
+        {
+            // arrange
+            var expectedFileContents = "What is this?\nI don't even--";
+            var filePath = $"{_rootDirectory}\\iDoNotExist.sad";
+            File.Create(filePath).Close();
+
+            using (var streamWriter = new StreamWriter(filePath))
+                streamWriter.Write(expectedFileContents);
+
+            // act
+            var actualFileContents = _fileReader.Read(filePath);
+
+            // assert
+            actualFileContents.Should().Be(expectedFileContents);
+        }
+
+        #endregion Tests: Read()
+    }
+}
