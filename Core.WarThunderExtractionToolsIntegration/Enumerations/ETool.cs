5ad9cf2f6aa325650c54ea5fc951ef8075b587fc
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-15 01:42:50 +0200

    fix: correct tool file names

--- Core.WarThunderExtractionToolsIntegration/Enumerations/ETool.cs
+++ Core.WarThunderExtractionToolsIntegration/Enumerations/ETool.cs
@@ -5,13 +5,14 @@ namespace Core.UnpackingToolsIntegration.Enumerations
     /// <summary> Enumerates names of unpacking tools. </summary>
     public class ETool
     {
-        private const string _unpacker = "_unpack";
+        private const string _unpack = "_unpack";
+        private const string _unpacker = _unpack + "er";
         private const string _exe = ECharacterString.Period + EFileExtension.Exe;
 
-        public const string BlkUnpacker = "blk" + _unpacker + _exe;
-        public const string ClogUnpacker = "clog" + _unpacker + _exe;
-        public const string DdsxUnpacker = "ddsx" + _unpacker + _exe;
-        public const string DxpUnpacker = "dxp" + _unpacker + _exe;
+        public const string BlkUnpacker = "blk" + _unpack + _exe;
+        public const string ClogUnpacker = "clog" + _unpack + _exe;
+        public const string DdsxUnpacker = "ddsx" + _unpack + _exe;
+        public const string DxpUnpacker = "dxp" + _unpack + _exe;
         public const string VromfsUnpacker = EFile.Vromfs + _unpacker + _exe;
         public const string WrplUnpacker = "wrpl" + _unpacker + _exe;
     }
