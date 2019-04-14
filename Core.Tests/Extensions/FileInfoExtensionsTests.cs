d0243115ace5c75f9f6277cbaf1e305c7c637cce
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-14 22:35:39 +0200

    test: add a UT for FileInfoExtensions.GetFileNameWithoutExtension()

---
+++ Core.Tests/Extensions/FileInfoExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Core.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Core.Tests.Extensions
+{
+    /// <summary> See <see cref="FileInfoExtensions"/>. </summary>
+    [TestClass]
+    public class FileInfoExtensionsTests
+    {
+        #region Tests: GetFileNameWithoutExtension()
+
+        [TestMethod]
+        public void GetFileNameWithoutExtension()
+        {
+            // arrange
+            var expectedFileNameWithoutExtension = "whatsitsface";
+            var fileInfo = new FileInfo($@"{Directory.GetCurrentDirectory()}\{expectedFileNameWithoutExtension}.gif");
+
+            // act
+            var actualFileNameWithoutExtension = fileInfo.GetFileNameWithoutExtension();
+
+            // assert
+            actualFileNameWithoutExtension.Should().Be(expectedFileNameWithoutExtension);
+        }
+
+        #endregion Tests: GetFileNameWithoutExtension()
+    }
+}
