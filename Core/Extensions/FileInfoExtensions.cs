ebcafa1f67210e200baf5fff1a5636913b625787
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-14 17:46:45 +0200

    feature: implement FileInfoExtensions.GetFileNameWithoutExtension()

---
+++ Core/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enumerations;
+using System.IO;
+
+namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="FileInfo"/> class. </summary>
+    public static class FileInfoExtensions
+    {
+        /// <summary> Get the file name without an extension (if any). </summary>
+        /// <param name="fileInfo"> A source file information instance. </param>
+        /// <returns></returns>
+        public static string GetFileNameWithoutExtension(this FileInfo fileInfo) =>
+            fileInfo.Name.Contains(ECharacter.Period)
+                ? fileInfo.Name.Split(ECharacter.Period).SkipLast(1).StringJoin()
+                : fileInfo.Name;
+    }
+}
