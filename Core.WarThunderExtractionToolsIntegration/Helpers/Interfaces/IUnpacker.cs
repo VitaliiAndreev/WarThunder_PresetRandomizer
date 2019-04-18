c936071b1eaea50e5ae48f5eb263f106980f6bad
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-18 23:03:10 +0200

    feature: implement an Unpacker.Unpack() variant that accepts directories to unpack files en masse but without copying them; it is needed to unpack .blk files stored in .bin files - to unpack the entirety of the package at once

--- Core.WarThunderExtractionToolsIntegration/Helpers/Interfaces/IUnpacker.cs
+++ Core.WarThunderExtractionToolsIntegration/Helpers/Interfaces/IUnpacker.cs
@@ -6,9 +6,18 @@ namespace Core.UnpackingToolsIntegration.Helpers.Interfaces
     /// <summary> Provides methods to unpack War Thunder files. </summary>
     public interface IUnpacker
     {
+        #region Method: Unpack()
+
         /// <summary> Copies the file to unpack into the <see cref="Settings.TempLocation"/> and unpacks it. </summary>
         /// <param name="sourceFile"> The file to unpack. </param>
         /// <returns> The path to the output file / directory. </returns>
         string Unpack(FileInfo sourceFile);
+
+        /// <summary> Unpacks all valid files in the specified folder with the given tool. </summary>
+        /// <param name="sourceDirectory"> The directory to unpack unpack files from. </param>
+        /// <param name="toolName"> The name of the unpacking tool to use. </param>
+        void Unpack(DirectoryInfo sourceDirectory, string toolName);
+
+        #endregion Method: Unpack()
     }
 }
--- Core.WarThunderExtractionToolsIntegration/Helpers/Unpacker.cs
+++ Core.WarThunderExtractionToolsIntegration/Helpers/Unpacker.cs
@@ -152,6 +152,8 @@ namespace Core.UnpackingToolsIntegration.Helpers
             return outputPath;
         }
 
+        #region Method: Unpack()
+
         /// <summary> Copies the file to unpack into the <see cref="Settings.TempLocation"/> and unpacks it. </summary>
         /// <param name="sourceFile"> The file to unpack. </param>
         /// <returns> The path to the output file / directory. </returns>
@@ -184,5 +186,25 @@ namespace Core.UnpackingToolsIntegration.Helpers
                 return null;
             }
         }
+
+        /// <summary> Unpacks all valid files in the specified folder with the given tool. </summary>
+        /// <param name="sourceDirectory"> The directory to unpack unpack files from. </param>
+        /// <param name="toolName"> The name of the unpacking tool to use. </param>
+        public void Unpack(DirectoryInfo sourceDirectory, string toolName)
+        {
+            LogDebug(ECoreLogMessage.Unpacking.FormatFluently(sourceDirectory.FullName));
+
+            try
+            {
+                RunShellCommand(GetToolFileInfo(toolName).FullName, sourceDirectory.FullName);
+                LogDebug(ECoreLogMessage.Unpacked.FormatFluently(sourceDirectory.Name));
+            }
+            catch (Exception exception)
+            {
+                LogErrorAndRethrow(exception);
+            }
+        }
+
+        #endregion Method: Unpack()
     }
 }
