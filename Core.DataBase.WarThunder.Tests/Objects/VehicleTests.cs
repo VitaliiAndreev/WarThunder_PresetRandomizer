2313c03557a4f3301e51a825797847868ced1d8c
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-22 00:07:47 +0200

    test: add 3 UTs for Vehicle

---
+++ Core.DataBase.WarThunder.Tests/Objects/VehicleTests.cs
@@ -0,0 +1,87 @@
+﻿using Core.DataBase.Helpers;
+using Core.DataBase.Tests;
+using Core.DataBase.Tests.Enumerations;
+using Core.DataBase.WarThunder.Objects;
+using Core.DataBase.WarThunder.Objects.Interfaces;
+using Core.Helpers.Logger.Enumerations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.DataBase.WarThunder.Tests.Objects
+{
+    /// <summary> See <see cref="Vehicle"/>.</summary>
+    [TestClass]
+    public class VehicleTests
+    {
+        #region Internal Methods
+
+        public override string ToString() => nameof(NationTests);
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Presets.Logger.LogInfo(ECoreLogCategory.UnitTests, ECoreLogMessage.CleanUpAfterUnitTestStartsHere);
+            Presets.CleanUp();
+        }
+
+        #endregion Internal Methods
+        #region Tests: CommitChanges() and Query()
+
+        [TestMethod]
+        public void CommitChanges_Query_ReturnsObject()
+        {
+            // arrange
+            var fileName = $"{ToString()}.{MethodBase.GetCurrentMethod().Name}()";
+
+            using (var dataRepository = new DataRepository(fileName, true, Assembly.Load(EAssemblies.WarThunderMappingAssembly), Presets.Logger))
+            {
+                var wingedBicycle = new Vehicle(dataRepository, "Winged Bycicle");
+
+                // act
+                dataRepository.PersistNewObjects();
+
+                var query = dataRepository.Query<IVehicle>();
+                query.Count().Should().Be(1);
+
+                var vehicle = query.First();
+
+                // assert
+                vehicle.IsEquivalentTo(wingedBicycle, 2);
+            }
+        }
+
+        #endregion Tests: CommitChanges() and Query()
+        #region Tests: IsEquivalentTo()
+
+        [TestMethod]
+        public void IsEquivalentTo_Self_ShouldBeTrue()
+        {
+            // arrange
+            var wingedBicycle = new Vehicle(Presets.MockDataRepository.Object, "Winged Bycicle");
+
+            // act
+            var isEquivalent = wingedBicycle.IsEquivalentTo(wingedBicycle, 2);
+
+            // assert
+            isEquivalent.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsEquivalentTo_AnotherInstance_ShouldBeFalse()
+        {
+            // arrange
+            var wingedBicycle = new Vehicle(Presets.MockDataRepository.Object, "Winged Bycicle");
+            var trackedWheelchair = new Vehicle(Presets.MockDataRepository.Object, "Tracked Wheelchair");
+
+            // act
+            var isEquivalent = wingedBicycle.IsEquivalentTo(trackedWheelchair, 2);
+
+            // assert
+            isEquivalent.Should().BeFalse();
+        }
+
+        #endregion Tests: IsEquivalentTo()
+    }
+}
