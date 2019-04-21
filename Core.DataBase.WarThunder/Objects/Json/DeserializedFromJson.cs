e6cb13421aa5410f125be1a2ccb9198d2726c33e
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-21 14:49:18 +0200

    feature: implement IDeserializedFromJson as the base for War Thunder specific JSON mapping classes

---
+++ Core.DataBase.WarThunder/Objects/Json/DeserializedFromJson.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataBase.WarThunder.Objects.Json.Interfaces;
+
+namespace Core.DataBase.WarThunder.Objects.Json
+{
+    /// <summary> The base class for mapping entities used for automated deserialization of JSON data before passing it on into persistent objects. </summary>
+    public class DeserializedFromJson : IDeserializedFromJson
+    {
+        /// <summary> The entity's Gaijin ID. </summary>
+        public string GaijinId { get; set; }
+    }
+}
---
+++ Core.DataBase.WarThunder/Objects/Json/Interfaces/IDeserializedFromJson.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DataBase.WarThunder.Objects.Json.Interfaces
+{
+    /// <summary> The base interface for mapping entities used for automated deserialization of JSON data before passing it on into persistent objects. </summary>
+    public interface IDeserializedFromJson
+    {
+        /// <summary> The entity's Gaijin ID. </summary>
+        string GaijinId { get; set; }
+    }
+}
