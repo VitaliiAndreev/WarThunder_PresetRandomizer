0688b547de9b863754b59ed3312817a01f70cb23
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-21 15:37:14 +0200

    chore: correct log messages

--- Core/Helpers/Logger/Enumerations/ECoreLogMessage.cs
+++ Core/Helpers/Logger/Enumerations/ECoreLogMessage.cs
@@ -559,13 +559,13 @@
         /// <para> 1: character count. </para>
         /// <para> 2: object type. </para>
         /// </summary>
-        public const string TryingToDeserializeJsonStringIntoObject = _TryingTo + _deserialize + _a + _SPC + _JSON + _string + _of + _SPC_FMT + _characters + _into + _an + _instance + _of + _SPC_FMT_Q + _FS;
+        public const string TryingToDeserializeJsonStringIntoObject = _TryingTo + _deserialize + _a + _JSON + _string + _of + _SPC_FMT + _characters + _into + _an + _instance + _of + _SPC_FMT_Q + _FS;
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: character count. </para>
         /// <para> 2: object type. </para>
         /// </summary>
-        public const string TryingToDeserializeJsonStringIntoCollection = _TryingTo + _deserialize + _a + _SPC + _JSON + _string + _of + _SPC_FMT + _characters + _into + _a + _collection + _of + _SPC_FMT_Q + _instances + _FS;
+        public const string TryingToDeserializeJsonStringIntoCollection = _TryingTo + _deserialize + _a + _JSON + _string + _of + _SPC_FMT + _characters + _into + _a + _collection + _of + _SPC_FMT_Q + _instances + _FS;
         public const string DeserializedInstance = _An + _instance + _has_been + _successfully + _deserialized + _FS;
         /// <summary>
         /// A message with formatting placeholders.
