1edbaf23c5743c672e718a48612f50df77137237
Vitalii Andreev <coverthelirush@gmail.com>
2019-04-23 23:52:42 +0200

    chore: revert early implementation

--- Core.DataBase.WarThunder/Objects/Interfaces/IVehicle.cs
+++ Core.DataBase.WarThunder/Objects/Interfaces/IVehicle.cs
@@ -5,10 +5,5 @@ namespace Core.DataBase.WarThunder.Objects.Interfaces
     /// <summary> A vehicle (air, ground, or sea). </summary>
     public interface IVehicle : IPersistentObjectWithIdAndGaijinId
     {
-        /// <summary>
-        /// The purchase cost in Silver Lions.
-        /// Zero means that the vehicle cannot be bought for Silver Lions.
-        /// </summary>
-        int PurchaseCostInSilver { get; }
     }
 }
