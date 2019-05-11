85eaf4c75fa166297120830ad4767aa66b2c7d88
Vitalii Andreev <coverthelirush@gmail.com>
2019-05-11 15:06:51 +0200

    feature: implement JsonTextReaderExtensions.ReadPastNonTokens()

---
+++ Core.Json/Extensions/JsonTextReaderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.Json.Extensions
+{
+    /// <summary> Methods extending the <see cref="JsonTextReader"/> class. </summary>
+    public static class JsonTextReaderExtensions
+    {
+        /// <summary> Reads through JSON text until an actual JSON token is found. </summary>
+        /// <param name="jsonTextReader"> A JSON text reader. </param>
+        public static void ReadPastNonTokens(this JsonTextReader jsonTextReader)
+        {
+            while (jsonTextReader.TokenType == JsonToken.None)
+                jsonTextReader.Read();
+        }
+    }
+}
