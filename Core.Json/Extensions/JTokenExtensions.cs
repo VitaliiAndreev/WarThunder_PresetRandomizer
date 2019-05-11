e40d4d36875c12757ba1b96b69d3cec412d28f70
Vitalii Andreev <coverthelirush@gmail.com>
2019-05-11 15:07:28 +0200

    feature: implement JTokenExtensions.CovertToArray()

---
+++ Core.Json/Extensions/JTokenExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Core.Json.Extensions
+{
+    /// <summary> Methods extending the <see cref="JToken"/> class. </summary>
+    public static class JTokenExtensions
+    {
+        /// <summary> Converts the JSON token into a JSON array with the token put into it. </summary>
+        /// <param name="jsonToken"> A JSON token to convert. </param>
+        /// <returns></returns>
+        public static JArray CovertToArray(this JToken jsonToken)
+        {
+            var jsonArray = new JArray();
+
+            if (jsonToken.Parent is JProperty jsonProperty)
+            {
+                jsonProperty.Value = jsonArray;
+                jsonArray.Add(jsonToken);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            return jsonArray;
+        }
+    }
+}
