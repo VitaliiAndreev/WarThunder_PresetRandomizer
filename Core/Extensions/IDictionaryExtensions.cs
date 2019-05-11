a1a024de253502208d77585f8077e8b98dbc0392
Vitalii Andreev <coverthelirush@gmail.com>
2019-05-11 15:15:24 +0200

    feature: implement IDictionaryExtensions.Copy<T, U>()

---
+++ Core/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="IDictionary{TKey, TValue}"/> class. </summary>
+    public static class IDictionaryExtensions
+    {
+        /// <summary> Creates a copy of the dictionary, i.e. a new dictionary with same contents. </summary>
+        /// <typeparam name="T"> A generic type. </typeparam>
+        /// <typeparam name="U"> A generic type. </typeparam>
+        /// <param name="sourceDictionary"> A source dictionary. </param>
+        /// <returns></returns>
+        public static IDictionary<T, U> Copy<T, U>(this IDictionary<T, U> sourceDictionary)
+        {
+            var newDictionary = new Dictionary<T, U>();
+
+            foreach (var keyValuePair in sourceDictionary)
+                newDictionary.Add(keyValuePair.Key, keyValuePair.Value);
+
+            return newDictionary;
+        }
+    }
+}
