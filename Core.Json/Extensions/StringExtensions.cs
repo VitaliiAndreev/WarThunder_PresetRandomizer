1d60e1a29329c905cda792fbdcbbb6a0cb340ae3
Vitalii Andreev <coverthelirush@gmail.com>
2019-05-12 21:06:44 +0200

    feature: implement StringExtensions.CreateJsonReader()

---
+++ Core.Json/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Core.Extensions;
+using Newtonsoft.Json;
+
+namespace Core.Json.Extensions
+{
+    /// <summary> Methods extending the <see cref="string"/> class. </summary>
+    public static class StringExtensions
+    {
+        /// <summary> Creates a new JSON reader with the given string. </summary>
+        /// <param name="sourceString"> A source string. </param>
+        /// <returns></returns>
+        public static JsonReader CreateJsonReader(this string sourceString) =>
+            new JsonTextReader(sourceString.CreateTextReader());
+    }
+}
