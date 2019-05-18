eaf75e92ac51ee17384e8f7dfb254ac0d7004dff
Vitalii Andreev <coverthelirush@gmail.com>
2019-05-18 19:37:57 +0200

    feature: implement JObjectExtensions.GetPropertyNames()

---
+++ Core.Json/Extensions/JObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Json.Extensions
+{
+    /// <summary> Methods extending the <see cref="JObject"/> class. </summary>
+    public static class JObjectExtensions
+    {
+        /// <summary> Returns a collection of the JSON object's property names. It is equivalent to <see cref="IDictionary{TKey, TValue}.Keys"/> </summary>
+        /// <param name="jsonObject"> A source JSON object. </param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetPropertyNames(this JObject jsonObject) =>
+            jsonObject.Children<JProperty>().Select(jsonProperty => jsonProperty.Name);
+    }
+}
