121b78d46d25d56f7f9cb2d8ca91b28edfd1bc49
Vitalii Andreev <coverthelirush@gmail.com>
2019-06-09 20:04:34 +0200

    chore: remove unused IDictionaryExtensions.Copy<T, U>()

--- Core/Extensions/IDictionaryExtensions.cs
+++ Core/Extensions/IDictionaryExtensions.cs
@@ -43,20 +43,5 @@ namespace Core.Extensions
         }
 
         #endregion Methods: Adding
-
-        /// <summary> Creates a copy of the dictionary, i.e. a new dictionary with same contents. </summary>
-        /// <typeparam name="T"> A generic type. </typeparam>
-        /// <typeparam name="U"> A generic type. </typeparam>
-        /// <param name="sourceDictionary"> A source dictionary. </param>
-        /// <returns></returns>
-        public static IDictionary<T, U> Copy<T, U>(this IDictionary<T, U> sourceDictionary)
-        {
-            var newDictionary = new Dictionary<T, U>();
-
-            foreach (var keyValuePair in sourceDictionary)
-                newDictionary.Add(keyValuePair.Key, keyValuePair.Value);
-
-            return newDictionary;
-        }
     }
 }
