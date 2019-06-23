5a22e6f4967d173914e740c12a534216e51dbddd
Vitalii Andreev <coverthelirush@gmail.com>
2019-06-23 14:29:13 +0200

    feature: implement IWarThunderFileManager

---
+++ Core.UnpackingToolsIntegration/Helpers/Interfaces/IWarThunderFileManager.cs
@@ -0,0 +1,15 @@
+﻿using Core.Helpers.Interfaces;
+using System.Collections.Generic;
+
+namespace Core.UnpackingToolsIntegration.Helpers.Interfaces
+{
+    public interface IWarThunderFileManager: IFileManager
+    {
+        /// <summary> Gets names of all <see cref="EFileExtension.SqLite3"/> database files for specific game versions. </summary>
+        /// <returns></returns>
+        IEnumerable<string> GetWarThunderDataBaseFileNames();
+
+        /// <summary> Removes all directories and files in <see cref="Settings.TempLocation"/>. </summary>
+        void CleanUpTempDirectory();
+    }
+}
---
+++ Core.UnpackingToolsIntegration/Helpers/WarThunderFileManager.cs
@@ -0,0 +1,44 @@
+﻿using Core.Enumerations;
+using Core.Extensions;
+using Core.Helpers;
+using Core.Helpers.Logger.Interfaces;
+using Core.UnpackingToolsIntegration.Helpers.Interfaces;
+using Core.WarThunderExtractionToolsIntegration;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Core.UnpackingToolsIntegration.Helpers
+{
+    /// <summary> Provides methods to manage files related to War Thunder. </summary>
+    public class WarThunderFileManager : FileManager, IWarThunderFileManager
+    {
+        #region Constructors
+
+        /// <summary> Creates a new database updater. </summary>
+        /// <param name="logger"> An instance of a logger. </param>
+        public WarThunderFileManager(IConfiguredLogger logger)
+            : base(logger)
+        {
+        }
+
+        #endregion Constructors
+
+        /// <summary> Gets names of all <see cref="EFileExtension.SqLite3"/> database files for specific game versions. </summary>
+        /// <returns></returns>
+        public IEnumerable<string> GetWarThunderDataBaseFileNames() =>
+            new DirectoryInfo(Directory.GetCurrentDirectory())
+                .GetFiles(file => file.GetExtensionWithoutPeriod() == EFileExtension.SqLite3 && file.GetNameWithoutExtension().Matches(ERegularExpression.VersionFull))
+                .Select(file => file.Name)
+            ;
+
+        /// <summary> Removes all directories and files in <see cref="Settings.TempLocation"/>. </summary>
+        public void CleanUpTempDirectory()
+        {
+            var tempDirectory = new DirectoryInfo(Settings.TempLocation);
+
+            if (tempDirectory.Exists)
+                EmptyDirectory(tempDirectory.FullName);
+        }
+    }
+}
