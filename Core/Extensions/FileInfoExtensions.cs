a3b262f3dfe07fcf4c92b48d1d14a51cca08cf4b
Vitalii Andreev <coverthelirush@gmail.com>
2019-06-23 14:24:04 +0200

    feature: implement FileInfoExtensions.GetExtensionWithoutPeriod()

--- Core/Extensions/FileInfoExtensions.cs
+++ Core/Extensions/FileInfoExtensions.cs
@@ -1,11 +1,22 @@
 ﻿using Core.Enumerations;
 using System.IO;
+using System.Linq;
 
 namespace Core.Extensions
 {
     /// <summary> Methods extending the <see cref="FileInfo"/> class. </summary>
     public static class FileInfoExtensions
     {
+        /// <summary> Get the file name without an extension (if any). </summary>
+        /// <param name="fileInfo"> A source file information instance. </param>
+        /// <returns></returns>
+        public static string GetExtensionWithoutPeriod(this FileInfo fileInfo) =>
+            fileInfo
+                .Extension
+                .Skip(1)
+                .StringJoin()
+            ;
+
         /// <summary> Get the file name without an extension (if any). </summary>
         /// <param name="fileInfo"> A source file information instance. </param>
         /// <returns></returns>
