e7c923ba9ebfb3dd85bfebca8edaa3bac4a5ba87
Vitalii Andreev <coverthelirush@gmail.com>
2019-06-23 18:40:38 +0200

    feature: implement Interval classes

---
+++ Core/Objects/Abstract/IntervalBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Objects.Abstract
+{
+    /// <summary> The abstract base class for type-specific intervals. </summary>
+    public abstract class IntervalBase
+    {
+        /// <summary> Whether the left endpoint is included in the interval. </summary>
+        public bool LeftBounded { get; }
+        /// <summary> Whether the right endpoint is included in the interval. </summary>
+        public bool RightBounded { get; }
+
+        /// <summary> Takes parameters from inheriting interval classes. </summary>
+        /// <param name="leftBounded"> Whether the left endpoint is included in the interval. </param>
+        /// <param name="rightBounded"> Whether the right endpoint is included in the interval. </param>
+        public IntervalBase(bool leftBounded, bool rightBounded)
+        {
+            LeftBounded = leftBounded;
+            RightBounded = rightBounded;
+        }
+    }
+}
---
+++ Core/Objects/IntervalDecimal.cs
@@ -0,0 +1,34 @@
+﻿using Core.Objects.Abstract;
+
+namespace Core.Objects
+{
+    /// <summary> An interval of decimal values. </summary>
+    public class IntervalDecimal : IntervalBase
+    {
+        /// <summary> The left endpoint of the interval. </summary>
+        public decimal LeftEndpoint { get; }
+        /// <summary> The right endpoint of the interval. </summary>
+        public decimal RightEndpoint { get; }
+
+        /// <summary> Creates a new interval of decimal values. </summary>
+        /// <param name="leftBounded"> Whether the left endpoint is included in the interval. </param>
+        /// <param name="leftEndpoint"> The left endpoint of the interval. </param>
+        /// <param name="rightEndpoint"> The right endpoint of the interval. </param>
+        /// <param name="rightBounded"> Whether the right endpoint is included in the interval. </param>
+        public IntervalDecimal(bool leftBounded, decimal leftEndpoint, decimal rightEndpoint, bool rightBounded)
+            : base(leftBounded, rightBounded)
+        {
+            LeftEndpoint = leftEndpoint;
+            RightEndpoint = rightEndpoint;
+        }
+
+        /// <summary> Checks whether the given value is within the interval. </summary>
+        /// <param name="value"> The value to check. </param>
+        /// <returns></returns>
+        public bool Contains(decimal value)
+        {
+            return (LeftBounded ? value >= LeftEndpoint : value > LeftEndpoint)
+                && (RightBounded ? value <= RightEndpoint : value < RightEndpoint);
+        }
+    }
+}
