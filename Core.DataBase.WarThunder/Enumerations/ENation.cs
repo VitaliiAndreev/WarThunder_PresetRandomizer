0decab7d079bb978b4e0cad7a14f56fe9ac60add
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-13 13:02:54 +0200

    fix: add the missing ENation enumeration

---
+++ Core.DataBase.WarThunder/Enumerations/ENation.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.DataBase.WarThunder.Enumerations
+{
+    /// <summary> Nations available in War Thunder. </summary>
+    public enum ENation
+    {
+        None,
+        Usa,
+        Germany,
+        Ussr,
+        Commonwealth,
+        Japan,
+        Italy,
+        France,
+    }
+}
