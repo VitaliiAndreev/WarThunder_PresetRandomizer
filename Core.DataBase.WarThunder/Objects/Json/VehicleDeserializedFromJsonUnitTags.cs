0f55560ea80d7c243e40c59d9ffdecc82432a3de
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-13 13:08:30 +0200

    feature: implement JSON mapping for unittags.blkx

---
+++ Core.DataBase.WarThunder/Objects/Json/VehicleDeserializedFromJsonUnitTags.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.DataBase.WarThunder.Objects.Json
+{
+    /// <summary> A mapping entity used for automated deserialization of JSON data before passing it on into persistent objects. </summary>
+    public class VehicleDeserializedFromJsonUnitTags : DeserializedFromJson
+    {
+        /// <summary> The Gaijin ID of the vehicle's branch. </summary>
+        [JsonProperty("type", Required = Required.Always)]
+        public string BranchGaijinId { get; set; }
+
+        /// <summary> The Gaijin ID of the vehicle's country of origin (not the nation in whose research tree the vehicles is in). </summary>
+        [JsonProperty("originCountry")]
+        public string CountryGaijinId { get; set; }
+    }
+}
