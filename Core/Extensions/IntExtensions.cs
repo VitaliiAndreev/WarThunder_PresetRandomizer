3ff4d459a7feb2dab755694440c4db1d05ceeea8
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-23 13:48:02 +0200

    feature: implement IntExtensions.CastToDouble()

--- Core/Extensions/IntExtensions.cs
+++ Core/Extensions/IntExtensions.cs
@@ -5,6 +5,15 @@
     {
         #region Fluency
 
+        #region Type Casting
+
+        /// <summary> Directly casts the number into <see cref="double"/>. </summary>
+        /// <param name="number"> The source number. </param>
+        /// <returns></returns>
+        public static double CastToDouble(this int number) => (double)number;
+
+        #endregion Type Casting
+
         /// <summary> Checks whether the number is positive. </summary>
         /// <param name="value"> The number to check. </param>
         /// <returns></returns>
