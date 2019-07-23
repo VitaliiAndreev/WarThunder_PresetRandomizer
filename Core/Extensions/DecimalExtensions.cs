ea5654cc443f18f51bae67185c7cdc862f688839
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-23 17:58:44 +0200

    feature: implement DecimalExtensions.Floor()

---
+++ Core/Extensions/DecimalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Core.Objects;
+using System;
+
+namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="decimal"/> structure. </summary>
+    public static class DecimalExtensions
+    {
+        #region Fluency
+
+        #region Math
+
+        /// <summary> Returns the largest integer less than or equal to the specified decimal number. </summary>
+        /// <param name="number"> The number to process. </param>
+        /// <returns></returns>
+        public static decimal Floor(this decimal number) =>
+            Math.Floor(number);
+
+        #endregion Math
+
+        #endregion Fluency
+    }
+}
