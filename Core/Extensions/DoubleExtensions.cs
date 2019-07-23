889f381bc3411a7e20f7cd2e940d178734567df9
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-23 17:59:24 +0200

    feature: implement DoubleExtensions.CastToInt()

---
+++ Core/Extensions/DoubleExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="double"/> structure. </summary>
+    public static class DoubleExtensions
+    {
+        #region Fluency
+
+        #region Type Casting
+
+        /// <summary> Directly casts the number into <see cref="int"/>. </summary>
+        /// <param name="number"> The source number. </param>
+        /// <returns></returns>
+        public static int CastToInt(this double number) => (int)number;
+
+        #endregion Type Casting
+
+        #endregion Fluency
+    }
+}
