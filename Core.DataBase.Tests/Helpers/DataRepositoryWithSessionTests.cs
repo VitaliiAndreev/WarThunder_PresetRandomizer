5d98411b9314b5a9e7ebd9701b6599736d1f29ad
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-28 14:31:57 +0200

    test: add 1 UT for DataRepositoryWithSession

---
+++ Core.DataBase.Tests/Helpers/DataRepositoryWithSessionTests.cs
@@ -0,0 +1,59 @@
+﻿using Core.DataBase.Helpers;
+using Core.DataBase.Tests.Enumerations;
+using Core.DataBase.Tests.Mapping.OneClass.Id.Mapping;
+using Core.Enumerations.Logger;
+using Core.Tests;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.DataBase.Tests.Helpers
+{
+    /// <summary> See <see cref="DataRepositoryWithSession"/>. </summary>
+    [TestClass]
+    public class DataRepositoryWithSessionTests
+    {
+        #region Internal Methods
+
+        public override string ToString() => nameof(DataRepositoryWithSessionTests);
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Presets.Logger.LogInfo(ECoreLogCategory.UnitTests, ECoreLogMessage.CleanUpAfterUnitTestStartsHere);
+            Presets.CleanUp();
+        }
+
+        #endregion Internal Methods
+        #region Tests: CommitChanges() and Query()
+
+        [TestMethod]
+        public void CommitChanges_Query_ReturnsObject()
+        {
+            // arrange
+            var fileName = $"{ToString()}.{MethodBase.GetCurrentMethod().Name}()";
+
+            using (var dataRepository = new DataRepositoryWithSession(fileName, true, Assembly.Load(EAssembly.AssemblyWithMappingBase), Presets.Logger))
+            {
+                var id = -1L;
+
+                var fakeObject = new PersistentObjectFakeWithId(id);
+                fakeObject.InitializeNonPersistentFields(dataRepository);
+
+                dataRepository.NewObjects.Add(fakeObject);
+
+                // act
+                dataRepository.PersistNewObjects();
+
+                var query = dataRepository.Query<PersistentObjectFakeWithId>();
+
+                // assert
+                query.Count().Should().Be(1);
+                query.First().IsEquivalentTo(fakeObject, 1).Should().BeTrue();
+            }
+        }
+
+        #endregion Tests: CommitChanges() and Query()
+    }
+}
