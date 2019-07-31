80d3ba3037ca626fd009b9ad6bf5da1446b052ad
Vitalii Andreev <coverthelirush@gmail.com>
2019-07-31 13:42:15 +0200

    test: add 2 UTs for LocalizationManager

---
+++ Core.Localization.Tests/Helpers/LocalizationManagerTests.cs
@@ -0,0 +1,92 @@
+﻿using Core.Enumerations;
+using Core.Enumerations.Logger;
+using Core.Helpers;
+using Core.Helpers.Interfaces;
+using Core.Localization.Enumerations;
+using Core.Localization.Exceptions;
+using Core.Localization.Helpers;
+using Core.Tests;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Core.Localization.Tests.Helpers
+{
+    /// <summary> See <see cref="LocalizationManager"/>. </summary>
+    [TestClass]
+    public class LocalizationManagerTests
+    {
+        #region Fields
+
+        private IFileManager _fileManager;
+        private IFileReader _fileReader;
+
+        private string _localizationDirectory;
+
+        #endregion Fields
+        #region Internal Methods
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fileManager = new FileManager(Presets.Logger);
+            _fileReader = new FileReader(Presets.Logger);
+
+            _localizationDirectory = $@"{Directory.GetCurrentDirectory()}\{EWord.Localization}";
+
+            if (!Directory.Exists(_localizationDirectory))
+                Directory.CreateDirectory(_localizationDirectory);
+            else
+                _fileManager.EmptyDirectory(_localizationDirectory);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Presets.Logger.LogInfo(ECoreLogCategory.UnitTests, ECoreLogMessage.CleanUpAfterUnitTestStartsHere);
+            Presets.CleanUp();
+
+            _fileManager.DeleteDirectory(_localizationDirectory);
+        }
+
+        #endregion Internal Methods
+        #region Tests: Constructor()
+
+        [TestMethod]
+        public void Constructor_LanguageNotRecognized_ShouldThrow()
+        {
+            // arrange, act
+            Action createLocalizationManager = () => new LocalizationManager(_fileReader, "", Presets.Logger);
+
+            // assert
+            createLocalizationManager.Should().Throw<LanguageNotRecognizedException>();
+        }
+
+        [TestMethod]
+        public void Constructor_LanguageRecognized_ShouldLoad()
+        {
+            // arrange
+            var key = "SomethingSomething";
+            var value = "Ooh, it actually has something! :D";
+            var expectedFileContents = $@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<{EWord.Localization}>
+  <{EWord.Line} {EWord.Key_L}=""{key}"" {EWord.Value_L}=""{value}""/>
+</{EWord.Localization}>";
+
+            var filePath = $@"{_localizationDirectory}\English.xml";
+            File.Create(filePath).Close();
+
+            using (var streamWriter = new StreamWriter(filePath))
+                streamWriter.Write(expectedFileContents);
+
+            // act
+            var localizationManager = new LocalizationManager(_fileReader, ELocalization.English.ToString(), Presets.Logger);
+
+            // assert
+            localizationManager.GetValue(key).Should().Be(value);
+        }
+
+        #endregion Tests: Constructor()
+    }
+}
---
+++ Core.Localization.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core.Localization.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core.Localization.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("908b1d1f-7b02-433b-9454-82552b5547fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
