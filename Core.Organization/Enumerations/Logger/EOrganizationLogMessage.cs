9410e3e7de43a5eea4744ff60ca5150a82399704
Vitalii Andreev <coverthelirush@gmail.com>
2019-08-10 15:52:38 +0200

    refactor: replace properties with fields in LogMessage classes

--- Core.Organization/Enumerations/Logger/EOrganizationLogMessage.cs
+++ Core.Organization/Enumerations/Logger/EOrganizationLogMessage.cs
@@ -4,28 +4,28 @@ namespace Core.Organization.Enumerations.Logger
 {
     public class EOrganizationLogMessage : EDatabaseLogMessage
     {
-        public static string PreparingGameFiles => $"{_Preparing} {_game} {_files}.";
-        public static string GameFilesPrepared => $"{_Game} {_files} {_prepared}.";
+        public static string PreparingGameFiles = $"{_Preparing} {_game} {_files}.";
+        public static string GameFilesPrepared = $"{_Game} {_files} {_prepared}.";
 
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: game version. </para>
         /// </summary>
-        public static string FoundDatabaseFor => $"{_Found} {_databaseFor} {{0}}.";
+        public static string FoundDatabaseFor = $"{_Found} {_databaseFor} {{0}}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: game version. </para>
         /// </summary>
-        public static string NotFoundDatabaseFor => $"{_Not} {_found} {_databaseFor} {{0}}.";
+        public static string NotFoundDatabaseFor = $"{_Not} {_found} {_databaseFor} {{0}}.";
 
-        public static string CreatingDatabase => $"{_Creating} {_database}.";
-        public static string DatabaseCreatedConnectionEstablished => $"{_Database} {_created}. {_Connection} {_established}.";
-        public static string DataBaseConnectionEstablished => $"{_Database} {_connection} {_established}.";
+        public static string CreatingDatabase = $"{_Creating} {_database}.";
+        public static string DatabaseCreatedConnectionEstablished = $"{_Database} {_created}. {_Connection} {_established}.";
+        public static string DataBaseConnectionEstablished = $"{_Database} {_connection} {_established}.";
 
-        public static string InitializingDatabase => $"{_Initializing} {_database}.";
-        public static string DatabaseInitialized => $"{_Database} {_initialized}.";
+        public static string InitializingDatabase = $"{_Initializing} {_database}.";
+        public static string DatabaseInitialized = $"{_Database} {_initialized}.";
 
-        public static string CachingObjects => $"{_Caching} {_objects}.";
-        public static string CachingComplete => $"{_Caching} {_complete}.";
+        public static string CachingObjects = $"{_Caching} {_objects}.";
+        public static string CachingComplete = $"{_Caching} {_complete}.";
     }
 }
--- Core/Enumerations/Logger/ECoreLogMessage.cs
+++ Core/Enumerations/Logger/ECoreLogMessage.cs
@@ -5,27 +5,27 @@
     {
         #region Dispose()
 
-        public static string AlreadyClosed => $"{_Already} {_closed}.";
+        public static string AlreadyClosed = $"{_Already} {_closed}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: object. </para>
         /// </summary>
-        public static string IsNull_DisposalAborted => $"{{0}} {_is} {_NULL}. {_Disposal} {_aborted}.";
-        public static string Closing => $"{_Closing}.";
+        public static string IsNull_DisposalAborted = $"{{0}} {_is} {_NULL}. {_Disposal} {_aborted}.";
+        public static string Closing = $"{_Closing}.";
 
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: object. </para>
         /// </summary>
-        public static string PreparingToDisposeOf => $"{_Preparing} {_to} {_dispose} {_of} {{0}}.";
-        public static string AlreadyDisposed => $"{_Already} {_disposed} {_of}.";
-        public static string Disposing => $"{_Disposing}.";
-        public static string SuccessfullyDisposed => $"{_Successfully} {_disposed} {_of}.";
+        public static string PreparingToDisposeOf = $"{_Preparing} {_to} {_dispose} {_of} {{0}}.";
+        public static string AlreadyDisposed = $"{_Already} {_disposed} {_of}.";
+        public static string Disposing = $"{_Disposing}.";
+        public static string SuccessfullyDisposed = $"{_Successfully} {_disposed} {_of}.";
 
         #endregion Dispose()
         #region Exception Formatter
 
-        public static string ExceptionIsNull => $"{_Exception} {_is} {_NULL}.";
+        public static string ExceptionIsNull = $"{_Exception} {_is} {_NULL}.";
 
         #endregion Exception Formatter
         #region File Manager
@@ -35,104 +35,104 @@
         /// <para> 1: directory path / file name / etc. </para>
         /// <para> 2: destination directory path. </para>
         /// </summary>
-        public static string Copying => $"{_Copying} \"{{0}}\" {_into} \"{{1}}\".";
+        public static string Copying = $"{_Copying} \"{{0}}\" {_into} \"{{1}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path / file name / etc. </para>
         /// <para> 2: destination directory path. </para>
         /// </summary>
-        public static string Copied => $"\"{{0}}\" {_copied}.";
-        public static string Overwriting => $"{_Overwriting}.";
+        public static string Copied = $"\"{{0}}\" {_copied}.";
+        public static string Overwriting = $"{_Overwriting}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path / file name / etc. </para>
         /// </summary>
-        public static string Deleting => $"{_Deleting} \"{{0}}\".";
+        public static string Deleting = $"{_Deleting} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string DeletingEmptyDirectory => $"{_Deleting} {_empty} \"{{0}}\".";
+        public static string DeletingEmptyDirectory = $"{_Deleting} {_empty} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string Deleted => $"\"{{0}}\" {_deleted}.";
+        public static string Deleted = $"\"{{0}}\" {_deleted}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string EmptyingDirectory => $"{_Emptying} \"{{0}}\".";
+        public static string EmptyingDirectory = $"{_Emptying} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string DirectoryEmptied => $"{{0}}\" {_emptied}.";
+        public static string DirectoryEmptied = $"{{0}}\" {_emptied}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string SelectingAllFilesFromDirectory => $"{_Selecting} {_all} {_files} {_from} \"{{0}}\".";
+        public static string SelectingAllFilesFromDirectory = $"{_Selecting} {_all} {_files} {_from} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file extensions. </para>
         /// </summary>
-        public static string FilteringFilesFromSelection => $"{_Filtering} \"{{0}}\" {_files} {_from} {_selection}.";
+        public static string FilteringFilesFromSelection = $"{_Filtering} \"{{0}}\" {_files} {_from} {_selection}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file count. </para>
         /// </summary>
-        public static string SelectedFileCount => $"{_Selected} {{0}} {_file}({_s}).";
+        public static string SelectedFileCount = $"{_Selected} {{0}} {_file}({_s}).";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: file count. </para>
         /// </summary>
-        public static string DeletingFiles => $"{_Deleting} {{0}} {_file}({_s}).";
-        public static string FileDeleted => $"{_File} {_deleted}.";
-        public static string FilesDeleted => $"{_All} {_files} {_deleted}.";
-        public static string CheckingSubdirectories => $"{_Checking} {_for} {_subdirectories}.";
+        public static string DeletingFiles = $"{_Deleting} {{0}} {_file}({_s}).";
+        public static string FileDeleted = $"{_File} {_deleted}.";
+        public static string FilesDeleted = $"{_All} {_files} {_deleted}.";
+        public static string CheckingSubdirectories = $"{_Checking} {_for} {_subdirectories}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: subfolder count. </para>
         /// </summary>
-        public static string SubdirectoriesFound => $"{{0}} {_subdirectories} {_found}.";
+        public static string SubdirectoriesFound = $"{{0}} {_subdirectories} {_found}.";
 
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory / file path. </para>
         /// </summary>
-        public static string AlreadyExists_CopyingSkipped => $"\"{{0}}\" {_already} {_exists}. {_Copying} {_skipped}.";
+        public static string AlreadyExists_CopyingSkipped = $"\"{{0}}\" {_already} {_exists}. {_Copying} {_skipped}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory / file path. </para>
         /// </summary>
-        public static string DoesNotExist_NoNeedToDelete => $"{DoesntExist} {_No} {_need} {_to} {_delete} {_it}.";
+        public static string DoesNotExist_NoNeedToDelete = $"{DoesntExist} {_No} {_need} {_to} {_delete} {_it}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory / file path. </para>
         /// </summary>
-        public static string DoesNotExist_CopyingAborted => $"{DoesntExist} {_Copying} {_safely} {_aborted}.";
+        public static string DoesNotExist_CopyingAborted = $"{DoesntExist} {_Copying} {_safely} {_aborted}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: destination directory. </para>
         /// <para> 2: directory / file path. </para>
         /// </summary>
-        public static string DoesNotExist_CopyingSomethingAborted => $"{DoesntExist} {_Copying} \"{{0}}\" {_safely} {_aborted}.";
+        public static string DoesNotExist_CopyingSomethingAborted = $"{DoesntExist} {_Copying} \"{{0}}\" {_safely} {_aborted}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory path. </para>
         /// </summary>
-        public static string DirectoryDoesNotExist_DeletingAborted => $"{_The} {_directory} \"{{0}}\" {_doesnt} {_exist}. {_Deleting} {_files} {_safely} {_aborted}.";
+        public static string DirectoryDoesNotExist_DeletingAborted = $"{_The} {_directory} \"{{0}}\" {_doesnt} {_exist}. {_Deleting} {_files} {_safely} {_aborted}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory name. </para>
         /// </summary>
-        public static string DirectoryIsEmpty => $"{_The} {_directory} \"{{0}}\" {_is} {_empty}.";
-        public static string NoFilesOfSpecifiedFormatToDelete => $"{_No} {_files} {_of} {_specified} {_format} {_to} {_delete}.";
-        public static string NoSubdirectories => $"{_No} {_subdirectories}.";
+        public static string DirectoryIsEmpty = $"{_The} {_directory} \"{{0}}\" {_is} {_empty}.";
+        public static string NoFilesOfSpecifiedFormatToDelete = $"{_No} {_files} {_of} {_specified} {_format} {_to} {_delete}.";
+        public static string NoSubdirectories = $"{_No} {_subdirectories}.";
 
-        public static string ErrorDeletingFile => $"{_Error} {_deleting} {_file}.";
-        public static string ErrorDeletingFiles => $"{_Error} {_deleting} {_one} {_of} {_the} {_files}.";
+        public static string ErrorDeletingFile = $"{_Error} {_deleting} {_file}.";
+        public static string ErrorDeletingFiles = $"{_Error} {_deleting} {_one} {_of} {_the} {_files}.";
 
         #endregion File Manager
         #region File Reader
@@ -141,25 +141,25 @@
         /// A message with formatting placeholders.
         /// <para> 1: directory path / file name / etc. </para>
         /// </summary>
-        public static string NotFound => $"\"{{0}}\" {_not} {_found}.";
+        public static string NotFound = $"\"{{0}}\" {_not} {_found}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path / file name / etc. </para>
         /// </summary>
-        public static string CreatingStreamReader => $"{_Creating} {_a} {_stream} {_reader} {_from} \"{{0}}\".";
+        public static string CreatingStreamReader = $"{_Creating} {_a} {_stream} {_reader} {_from} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: directory path / file name / etc. </para>
         /// </summary>
-        public static string Reading => $"{_Reading} \"{{0}}\".";
-        public static string StreamReaderCreated => $"{_Stream} {_reader} {_created}.";
+        public static string Reading = $"{_Reading} \"{{0}}\".";
+        public static string StreamReaderCreated = $"{_Stream} {_reader} {_created}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: character count. </para>
         /// </summary>
-        public static string ReadCharacters => $"{_Read} {{0}} {_characters}.";
+        public static string ReadCharacters = $"{_Read} {{0}} {_characters}.";
 
-        public static string ErrorReadingFile => $"{_Error} {_reading} {_file}.";
+        public static string ErrorReadingFile = $"{_Error} {_reading} {_file}.";
 
         #endregion File Reader
         #region General
@@ -170,38 +170,38 @@
         /// A message with formatting placeholders.
         /// <para> 1: directory / file name. </para>
         /// </summary>
-        public static string Creating_InQuotes => $"{_Creating} \"{{0}}\".";
+        public static string Creating_InQuotes = $"{_Creating} \"{{0}}\".";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: object. </para>
         /// </summary>
-        public static string Created => $"{{0}} {_created}.";
+        public static string Created = $"{{0}} {_created}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory / file name. </para>
         /// </summary>
-        public static string Created_InQuotes => $"\"{{0}}\" {_created}.";
+        public static string Created_InQuotes = $"\"{{0}}\" {_created}.";
         
-        public static string AnErrorHasOccurred => $"{_An} {_error} {_has} {_occured}.";
+        public static string AnErrorHasOccurred = $"{_An} {_error} {_has} {_occured}.";
         /// <summary>
         /// A message with formatting placeholders.
         /// <para> 1: directory/file path/name. </para>
         /// </summary>
-        public static string DoesntExist => $"\"{{0}}\" {_doesnt} {_exist}.";
+        public static string DoesntExist = $"\"{{0}}\" {_doesnt} {_exist}.";
 
         #endregion General
         #region Parser
 
-        public static string ReadingClientVersion => $"{_Reading} {_the} {_client} {_version}.";
+        public static string ReadingClientVersion = $"{_Reading} {_the} {_client} {_version}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: client version. </para>
         /// </summary>
-        public static string ClientVersionIs => $"{_Client} {_version} {_is} {{0}}.";
+        public static string ClientVersionIs = $"{_Client} {_version} {_is} {{0}}.";
 
-        public static string VersionNotFoundInSourceString => $"{_Client} {_version} {_markers} {_not} {_found} {_in} {_the} {_source} {_string}.";
-        public static string ErrorReadingRawInstallData => $"{_Error} {_reading} {_raw} {_install} {_data}.";
-        public static string ErrorParsingVersionString => $"{_Error} {_parsing} {_version} {_string}.";
+        public static string VersionNotFoundInSourceString = $"{_Client} {_version} {_markers} {_not} {_found} {_in} {_the} {_source} {_string}.";
+        public static string ErrorReadingRawInstallData = $"{_Error} {_reading} {_raw} {_install} {_data}.";
+        public static string ErrorParsingVersionString = $"{_Error} {_parsing} {_version} {_string}.";
 
         #endregion Parser
         #region Settings Manager
@@ -210,15 +210,15 @@
         /// A message with formatting placeholders.
         /// <para> 1: XML node name. </para>
         /// </summary>
-        public static string XmlNodeNotFound => $"{{0}} {_XML} {_node} {_not} {_found}.";
+        public static string XmlNodeNotFound = $"{{0}} {_XML} {_node} {_not} {_found}.";
 
         #endregion Settings Manager
         #region Unit Tests
 
         private const string _line = "====================";
 
-        public static string CleanUpAfterUnitTestStartsHere => _line + $"{_A} {_cleanup} {_after} {_the} {_unit} {_test} {_starts} {_here}. " + _line;
-        public static string CleanUpAfterIntegrationTestStartsHere => _line + $"{_A} {_cleanup} {_after} {_the} {_integration} {_test} {_starts} {_here}. " + _line;
+        public static string CleanUpAfterUnitTestStartsHere = _line + $"{_A} {_cleanup} {_after} {_the} {_unit} {_test} {_starts} {_here}. " + _line;
+        public static string CleanUpAfterIntegrationTestStartsHere = _line + $"{_A} {_cleanup} {_after} {_the} {_integration} {_test} {_starts} {_here}. " + _line;
 
         #endregion Unit Tests
         #region Unpacker
@@ -227,35 +227,35 @@
         /// A message with formatting placeholders.
         /// <para> 1: file name. </para>
         /// </summary>
-        public static string PreparingToUnpack => $"{_Preparing} {_to} {_unpack} \"{{0}}\".";
+        public static string PreparingToUnpack = $"{_Preparing} {_to} {_unpack} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file name. </para>
         /// </summary>
-        public static string Unpacking => $"{_Unpacking} \"{{0}}\".";
+        public static string Unpacking = $"{_Unpacking} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file name. </para>
         /// </summary>
-        public static string Unpacked => $"{_Unpacked} \"{{0}}\".";
+        public static string Unpacked = $"{_Unpacked} \"{{0}}\".";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: unpacking tool file name. </para>
         /// </summary>
-        public static string UnpackingToolSelected => $"{{0}} {_selected}.";
+        public static string UnpackingToolSelected = $"{{0}} {_selected}.";
 
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file extension. </para>
         /// </summary>
-        public static string FileExtensionNotSupportedByUnpackingTools => $"{_No} {_unpacking} {_tools} {_found} {_for} \"{{0}}\" {_files}.";
+        public static string FileExtensionNotSupportedByUnpackingTools = $"{_No} {_unpacking} {_tools} {_found} {_for} \"{{0}}\" {_files}.";
         /// <summary> 
         /// A message with formatting placeholders.
         /// <para> 1: file extension. </para>
         /// </summary>
-        public static string OutputPathGenerationForFileExtensionNotYetImplemented => $"{_Output} {_path} {_generation} {_for} \"{{0}}\" {_files} {_is} {_not} {_yet} {_implemented}.";
-        public static string ErrorMatchingUnpakingToolToFileExtension => $"{_Error} {_matching} {_an} {_unpacking} {_tool} {_to} {_a} {_file} {_extension}.";
-        public static string ErrorRunningUnpackingTool => $"{_Error} {_running} {_the} {_unpacking} {_tool}.";
+        public static string OutputPathGenerationForFileExtensionNotYetImplemented = $"{_Output} {_path} {_generation} {_for} \"{{0}}\" {_files} {_is} {_not} {_yet} {_implemented}.";
+        public static string ErrorMatchingUnpakingToolToFileExtension = $"{_Error} {_matching} {_an} {_unpacking} {_tool} {_to} {_a} {_file} {_extension}.";
+        public static string ErrorRunningUnpackingTool = $"{_Error} {_running} {_the} {_unpacking} {_tool}.";
 
         #endregion Unpacker
     }
