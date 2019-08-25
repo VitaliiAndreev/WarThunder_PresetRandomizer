ceea5d3a152b5b7c962133da473826c3e53258e8
Vitalii Andreev <coverthelirush@gmail.com>
2019-08-25 20:36:56 +0200

    feature: implement Core.NLogExtensions

---
+++ Core.NLogExtensions/Objects/LogEventInfoLaidOutForWpf.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+
+namespace Core.NLogExtensions.Objects
+{
+    /// <summary> A wrapper for <see cref="NLog.LogEventInfo"/> with a pre-set layout. </summary>
+    public class LogEventInfoLaidOutForWpf
+    {
+        #region Properties
+
+        /// <summary> The internal instance of the log event information. </summary>
+        public LogEventInfo LogEventInfo { get; }
+        
+        /// <summary> A ready-to-to message formed using a pre-set layout. </summary>
+        public string LaidOutMessage => $"{LogEventInfo.TimeStamp.TimeOfDay} {LogEventInfo.Level.Name.ToUpper()} / {LogEventInfo.FormattedMessage}";
+
+        #endregion Properties
+        #region Constructors
+
+        /// <summary> Creates a new wrapper for <see cref="LogEventInfo"/> with a pre-set layout. </summary>
+        /// <param name="logEventInfo"> The internal instance of the log event information. </param>
+        public LogEventInfoLaidOutForWpf(LogEventInfo logEventInfo)
+        {
+            LogEventInfo = logEventInfo;
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ Core.NLogExtensions/Targets/WpfTarget.cs
@@ -0,0 +1,23 @@
+﻿using NLog.Common;
+using NLog.Targets;
+using System;
+
+namespace Core.NLogExtensions.Targets
+{
+    /// <summary> A custom target made for WPF applications. </summary>
+    [Target("WpfTarget")]
+    public class WpfTarget : Target
+    {
+        /// <summary> The event of logging. </summary>
+        public event Action<AsyncLogEventInfo> Log;
+
+        /// <summary> Writes an asynchronous log event to the log target. </summary>
+        /// <param name="logEvent"> The asynchronous Log event to write out. </param>
+        protected override void Write(AsyncLogEventInfo logEvent)
+        {
+            base.Write(logEvent);
+
+            Log?.Invoke(logEvent);
+        }
+    }
+}
