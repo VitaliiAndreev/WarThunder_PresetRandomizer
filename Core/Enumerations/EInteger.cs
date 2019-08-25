a7e93ed0102e5090b07c9c2cb1d4c47301021c2e
Vitalii Andreev <coverthelirush@gmail.com>
2019-08-25 20:25:29 +0200

    feature: implement EInteger

---
+++ Core/Enumerations/EInteger.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Enumerations
+{
+    /// <summary> Integer constants. </summary>
+    public class EInteger
+    {
+        public class Number
+        {
+            public const int Ten = 10;
+            public const int Hundred = 100;
+        }
+
+        public class Time
+        {
+            public const int MillisecondsInSecond = 1000;
+        }
+    }
+}
