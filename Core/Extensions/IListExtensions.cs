df60fc9dfd9bbf3a2696869c3dc90487f5d9f8d4
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-01 16:01:03 +0200

    feature: implement IListExtensions.Add range<T>()

---
+++ Core/Extensions/IListExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="IList{T}"/> interface. </summary>
+    public static class IListExtensions
+    {
+        #region Methods: Fluency
+
+        /// <summary> Add items from the specified collection into the list in bulk. </summary>
+        /// <typeparam name="T"> The type of items in both collections. </typeparam>
+        /// <param name="list"> The source list. </param>
+        /// <param name="collection"> The donor collection, i.e. whose elements are to be added into the list. </param>
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                list.Add(item);
+        }
+
+        #endregion Methods: Fluency
+    }
+}
