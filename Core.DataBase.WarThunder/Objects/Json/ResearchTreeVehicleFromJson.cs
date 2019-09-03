d338635a46b2de5348da08214c76fe63fbad2fc3
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-03 23:31:34 +0200

    feature: implement ResearchTreeVehicleFromJson

---
+++ Core.DataBase.WarThunder/Objects/Json/ResearchTreeVehicleFromJson.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Core.DataBase.WarThunder.Objects.Json
+{
+    /// <summary> A mapping entity used for automated deserialization of JSON data before passing it on into persistent objects. </summary>
+    public class ResearchTreeVehicleFromJson : DeserializedFromJson
+    {
+        [JsonProperty("rank", Required = Required.Always)]
+        public int Rank { get; set; }
+
+        /// <summary> 1-based coordinates of the research tree grid this vehicle occupies with its <see cref="Rank"/>. </summary>
+        [JsonProperty("rankPosXY")]
+        public List<int> PresetCellCoordinatesWithinRank { get; set; }
+
+        /// <summary> 1-based coordinates of the research tree grid this vehicle occupies with its <see cref="Rank"/>. </summary>
+        public List<int> CellCoordinatesWithinRank { get; set; }
+
+        [JsonProperty("reqAir")]
+        public string RequiredVehicleGaijinId { get; set; }
+
+        /// <summary> A 0-based index of the vehicle in its research tree folder. </summary>
+        public int? FolderIndex { get; set; }
+
+        [JsonProperty("gift")]
+        public string CategoryOfHiddenVehicles { get; set; }
+
+        [JsonProperty("showOnlyWhenBought")]
+        public bool? ShowOnlyWhenBought { get; set; }
+
+        [JsonProperty("showByPlatform")]
+        public string PlatformGaijinIdVehicleIsRestrictedTo { get; set; }
+
+        [JsonProperty("hideByPlatform")]
+        public string PlatformGaijinIdVehicleIsHiddenOn { get; set; }
+
+        [JsonProperty("hideFeature")]
+        public string HideCondition { get; set; }
+
+        [JsonProperty("marketplaceItemdefId")]
+        public long? MarketplaceId { get; set; }
+    }
+}
