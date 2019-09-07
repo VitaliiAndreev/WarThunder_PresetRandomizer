c586726f2f1379e675cce0f7e64feffd78ee0c57
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-07 19:57:35 +0200

    feature: implement IntExtensions.IsNegative() and .IsZero(), replace literals with references

--- Core/Extensions/IntExtensions.cs
+++ Core/Extensions/IntExtensions.cs
@@ -1,15 +1,29 @@
-﻿namespace Core.Extensions
+﻿using Core.Enumerations;
+
+namespace Core.Extensions
 {
     /// <summary> Methods extending the <see cref="int"/> class. </summary>
     public static class IntExtensions
     {
         #region Fluency
 
+        /// <summary> Checks whether the number is negative. </summary>
+        /// <param name="value"> The number to check. </param>
+        /// <returns></returns>
+        public static bool IsNegative(this int value) =>
+            value < EInteger.Number.Zero;
+
         /// <summary> Checks whether the number is positive. </summary>
         /// <param name="value"> The number to check. </param>
         /// <returns></returns>
         public static bool IsPositive(this int value) =>
-            value > 0;
+            value > EInteger.Number.Zero;
+
+        /// <summary> Checks whether the number is zero. </summary>
+        /// <param name="value"> The number to check. </param>
+        /// <returns></returns>
+        public static bool IsZero(this int value) =>
+            value == EInteger.Number.Zero;
 
         #endregion Fluency
     }
