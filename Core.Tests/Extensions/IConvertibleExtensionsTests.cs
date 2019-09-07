f36e56237d3539f7aa94ef91d4e5f7390bdbc863
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-07 20:27:09 +0200

    test: add 3 UTs for IConvertibleExtensions.ConvertFromTo<T, U>()

---
+++ Core.Tests/Extensions/IConvertibleExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using Core.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Extensions
+{
+    /// <summary> See <see cref="IConvertibleExtensions"/>. </summary>
+    [TestClass]
+    public class IConvertibleExtensionsTests
+    {
+        #region Tests: ConvertFromTo()
+
+        [TestMethod]
+        public void ConvertFromTo_LongToInt()
+        {
+            // arrange
+            var source = 17L;
+
+            // act
+            var output = source.ConvertFromTo<long, int>();
+
+            // assert
+            output.Should().Be(17);
+        }
+
+        [TestMethod]
+        public void ConvertFromTo_DoubleToIntRoundedDown()
+        {
+            // arrange
+            var source = 17.4;
+
+            // act
+            var output = source.ConvertFromTo<double, int>();
+
+            // assert
+            output.Should().Be(17);
+        }
+
+        [TestMethod]
+        public void ConvertFromTo_DoubleToIntRoundedUp()
+        {
+            // arrange
+            var source = 17.5;
+
+            // act
+            var output = source.ConvertFromTo<double, int>();
+
+            // assert
+            output.Should().Be(18);
+        }
+
+        #endregion Tests: ConvertFromTo()
+    }
+}
