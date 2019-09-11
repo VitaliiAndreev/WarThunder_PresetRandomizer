fc1e46ee29a528877f361e88b046f176ca05ddce
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-11 22:55:10 +0200

    feature: implement GameModeSelectionControl, add a missing reference to DropCapButton

---
+++ Client.Wpf/Controls/GameModeSelectionControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.Wpf.Controls
+{
+    /// <summary> Interaction logic for GameModeSelectionControl.xaml. </summary>
+    public partial class GameModeSelectionControl : UserControl
+    {
+        #region Events
+
+        /// <summary> Occurs when the "Arcade" button is clicked. </summary>
+        public event RoutedEventHandler ArcadeButtonClick
+        {
+            add => _arcadeButton.Click += value;
+            remove => _arcadeButton.Click -= value;
+        }
+
+        /// <summary> Occurs when the "Realistic" button is clicked. </summary>
+        public event RoutedEventHandler RealisticButtonClick
+        {
+            add => _realisticButton.Click += value;
+            remove => _realisticButton.Click -= value;
+        }
+
+        /// <summary> Occurs when the "Realistic" button is clicked. </summary>
+        public event RoutedEventHandler SimulatorButtonClick
+        {
+            add => _simulatorButton.Click += value;
+            remove => _simulatorButton.Click -= value;
+        }
+
+        #endregion Events
+        #region Constuctors
+
+        /// <summary> Creates a new control. </summary>
+        public GameModeSelectionControl()
+        {
+            InitializeComponent();
+
+            ArcadeButtonClick += OnClick;
+            RealisticButtonClick += OnClick;
+            SimulatorButtonClick += OnClick;
+        }
+
+        #endregion Constuctors
+        #region Methods: Event Handlers
+
+        /// <summary> Adjust font weights to indicate the button pressed last. </summary>
+        /// <param name="sender"> The object that has triggered the event. A <see cref="Button"/> is expected. </param>
+        /// <param name="eventArguments"> Not used. </param>
+        private void OnClick(object sender, RoutedEventArgs eventArguments)
+        {
+            if (!(sender is Button button)) return;
+
+            var dropCapButtons = _buttonGrid.Children.OfType<DropCapButton>();
+            var clickedDropCapButton = dropCapButtons.First(dropCapButton => dropCapButton.EmbeddedButton.Equals(button));
+
+            clickedDropCapButton.FontWeight = FontWeights.Bold;
+
+            dropCapButtons
+                .Except(new DropCapButton[] { clickedDropCapButton })
+                .ToList()
+                .ForEach(dropCapButton => dropCapButton.FontWeight = FontWeights.Normal)
+            ;
+        }
+
+        #endregion Methods: Event Handlers
+    }
+}
