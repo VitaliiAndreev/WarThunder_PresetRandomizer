4d5cb62542adb645ac0a4f6e1cdbc2b3ddd04a64
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-12 23:17:49 +0200

    feature: implement MainWindow.OnGameModeButtonClick() and subscribe it to events

--- Client.Wpf/Windows/MainWindow.xaml.cs
+++ Client.Wpf/Windows/MainWindow.xaml.cs
@@ -3,9 +3,11 @@ using Client.Wpf.Enumerations.Logger;
 using Client.Wpf.Presenters.Interfaces;
 using Client.Wpf.Windows.Interfaces;
 using Client.Wpf.Windows.Interfaces.Base;
+using Core.DataBase.WarThunder.Enumerations;
 using Core.Enumerations.Logger;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Client.Wpf.Windows
 {
@@ -35,6 +37,10 @@ namespace Client.Wpf.Windows
             Localize();
 
             Application.Current.ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            _gameModeSelectionControl.ArcadeButtonClick += OnGameModeButtonClick;
+            _gameModeSelectionControl.RealisticButtonClick += OnGameModeButtonClick;
+            _gameModeSelectionControl.SimulatorButtonClick += OnGameModeButtonClick;
         }
 
         #endregion Constructor
@@ -51,6 +57,20 @@ namespace Client.Wpf.Windows
         /// <param name="eventArguments"> Not used. </param>
         private void OnClosed(object sender, EventArgs eventArguments) =>
             Log.Debug(ECoreLogMessage.Closed);
+
+        /// <summary> Selects the game mode whose button is pressed. </summary>
+        /// <param name="sender"> The object that has triggered the event. A <see cref="Button"/> is expected. </param>
+        /// <param name="eventArguments"> Not used. </param>
+        private void OnGameModeButtonClick(object sender, RoutedEventArgs eventArguments)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (!(button.Tag is EGameMode buttonGameMode))
+                return;
+
+            SelectGameMode(buttonGameMode);
+        }
 
         #endregion Methods: Event Handlers
 
