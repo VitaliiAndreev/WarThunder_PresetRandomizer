433e77334dc5ced10c890bc2be8447765cf60d95
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-15 01:54:56 +0200

    feature: implement overrides of Equals() and GetHashCode() for ResearchTreeCoordinatesWithinRank

--- Core.Organization/Objects/ResearchTreeCoordinatesWithinRank.cs
+++ Core.Organization/Objects/ResearchTreeCoordinatesWithinRank.cs
@@ -29,5 +29,38 @@
         }
 
         #endregion Constructors
+        #region Methods: Comparison
+
+        /// <summary> Determines whether the specified object is equal to the current object. </summary>
+        /// <param name="obj"> The object to compare with the current object. </param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ResearchTreeCoordinatesWithinRank anotherInstance))
+                return false;
+
+            return ColumnNumber == anotherInstance.ColumnNumber
+                && RowNumber == anotherInstance.RowNumber
+                && FolderIndex == anotherInstance.FolderIndex
+            ;
+        }
+
+        /// <summary> Serves as the default hash function. </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hash = 101;
+
+                hash = hash * 103 + ColumnNumber.GetHashCode();
+                hash = hash * 107 + RowNumber.GetHashCode();
+                hash = hash * 109 + (FolderIndex.HasValue ? FolderIndex.Value.GetHashCode() : -1.GetHashCode());
+
+                return hash;
+            }
+        }
+
+        #endregion Methods: Comparison
     }
 }
