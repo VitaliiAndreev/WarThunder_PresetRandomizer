65a08d4193881133bc41fac6e5952b57bec62009
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-15 01:55:33 +0200

    test: add 1 UT for ResearchTreeCoordinatesWithinRank.GetHashCode()

---
+++ Core.Organization.Tests/Objects/ResearchTreeCoordinatesWithinRankTests.cs
@@ -0,0 +1,41 @@
+﻿using Core.Organization.Objects;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Organization.Tests.Objects
+{
+    /// <summary> See <see cref="ResearchTreeCoordinatesWithinRank"/>. </summary>
+    [TestClass]
+    public class ResearchTreeCoordinatesWithinRankTests
+    {
+        #region Tests: GetHashCode()
+
+        [TestMethod]
+        public void GetHashCode_MustHaveNoCollisionsWithinReasonableLimits()
+        {
+            // arrange
+            var coordinateSets = new List<ResearchTreeCoordinatesWithinRank>();
+
+            for (var rowIndex = 0; rowIndex < 100; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < 10; columnIndex++)
+                {
+                    coordinateSets.Add(new ResearchTreeCoordinatesWithinRank(columnIndex, rowIndex, null));
+
+                    for (var folderIndex = 0; folderIndex < 10; folderIndex++)
+                        coordinateSets.Add(new ResearchTreeCoordinatesWithinRank(columnIndex, rowIndex, folderIndex));
+                }
+            }
+
+            // act
+            var hashCodes = coordinateSets.Select(set => set.GetHashCode());
+
+            // assert
+            hashCodes.Distinct().Count().Should().Be(hashCodes.Count());
+        }
+
+        #endregion Tests: GetHashCode()
+    }
+}
