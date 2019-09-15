95fba9012110ea15900b9812e841df0a0b82c873
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-15 16:36:24 +0200

    refactor: reduce code duplicity

--- Core.DataBase.WarThunder/Enumerations/DataBase/ETable.cs
+++ Core.DataBase.WarThunder/Enumerations/DataBase/ETable.cs
@@ -3,23 +3,26 @@
     /// <summary> Tables in the local database. </summary>
     internal class ETable
     {
-        private const string _vehicle = Vehicle + "_";
+        private const string _ = "_";
+        private const string _objectPrefix = "obj";
 
+        private const string _vehicles = "Vehicles";
+        private const string _vehicleTable = Vehicle + _;
         internal const string Branch = "objBranches";
-        internal const string Nation = "objNations";
-        internal const string Vehicle = "objVehicles";
-        internal const string VehicleAverageAward = _vehicle + "AverageAward";
-        internal const string VehicleBattleRating = _vehicle + "BattleRating";
-        internal const string VehicleBattleTime = _vehicle + "BattleTime";
-        internal const string VehicleBattleTimeAward = _vehicle + "BattleTimeAward";
-        internal const string VehicleEconomicRank = _vehicle + "EconomicRank";
-        internal const string VehicleNumberOfSpawns = _vehicle + "NumberOfSpawns";
-        internal const string VehicleRepairCost = _vehicle + "RepairCost";
-        internal const string VehicleRepairTimeWithCrew = _vehicle + "RepairTimeWithCrew";
-        internal const string VehicleRepairTimeWithoutCrew = _vehicle + "RepairTimeWithoutCrew";
-        internal const string VehicleResearchTreeData = _vehicle + "ResearchTreeData";
-        internal const string VehicleRewardMultiplier = _vehicle + "RewardMultiplier";
-        internal const string VehicleVisualPremiumRewardMultiplier = _vehicle + "VisualPremiumRewardMultiplier";
-        internal const string VehicleVisualRewardMultiplier = _vehicle + "VisualRewardMultiplier";
+        internal const string Nation = _objectPrefix + "Nations";
+        internal const string Vehicle = _objectPrefix + _vehicles;
+        internal const string VehicleAverageAward = _vehicleTable + "AverageAward";
+        internal const string VehicleBattleRating = _vehicleTable + "BattleRating";
+        internal const string VehicleBattleTime = _vehicleTable + "BattleTime";
+        internal const string VehicleBattleTimeAward = _vehicleTable + "BattleTimeAward";
+        internal const string VehicleEconomicRank = _vehicleTable + "EconomicRank";
+        internal const string VehicleNumberOfSpawns = _vehicleTable + "NumberOfSpawns";
+        internal const string VehicleRepairCost = _vehicleTable + "RepairCost";
+        internal const string VehicleRepairTimeWithCrew = _vehicleTable + "RepairTimeWithCrew";
+        internal const string VehicleRepairTimeWithoutCrew = _vehicleTable + "RepairTimeWithoutCrew";
+        internal const string VehicleResearchTreeData = _vehicleTable + "ResearchTreeData";
+        internal const string VehicleRewardMultiplier = _vehicleTable + "RewardMultiplier";
+        internal const string VehicleVisualPremiumRewardMultiplier = _vehicleTable + "VisualPremiumRewardMultiplier";
+        internal const string VehicleVisualRewardMultiplier = _vehicleTable + "VisualRewardMultiplier";
     }
 }
