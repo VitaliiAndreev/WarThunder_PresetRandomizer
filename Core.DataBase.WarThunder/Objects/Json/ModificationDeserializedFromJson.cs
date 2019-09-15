f4ff0f92b3a450f6625cb0840986245f27328a21
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-15 23:03:56 +0200

    feature: implement a stub of ModificationDeserializedFromJson

---
+++ Core.DataBase.WarThunder/Objects/Json/ModificationDeserializedFromJson.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.DataBase.WarThunder.Objects.Json
+{
+    public class ModificationDeserializedFromJson : DeserializedFromJsonWithOwner<VehicleDeserializedFromJsonWpCost>
+    {
+        [JsonProperty("costGold", Required = Required.Always)]
+        public int PurchaseCostInGold { get; set; }
+    }
+}
