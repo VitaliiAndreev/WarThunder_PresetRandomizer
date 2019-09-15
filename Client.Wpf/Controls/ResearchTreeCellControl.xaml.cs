de175be0e8d780c9b245feb230154e1cd21dae06
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-15 02:06:51 +0200

    feature: implement ResearchTreeCellControl

---
+++ Client.Wpf/Controls/ResearchTreeCellControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Core.DataBase.WarThunder.Objects.Interfaces;
+using System.Windows.Controls;
+
+namespace Client.Wpf.Controls
+{
+    /// <summary> Interaction logic for ResearchTreeCellControl.xaml. </summary>
+    public partial class ResearchTreeCellControl : UserControl
+    {
+        #region Constructors
+
+        /// <summary> Creates a new control. </summary>
+        public ResearchTreeCellControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        /// <summary> Adds a new control for the specified vehicle and adds it to the the cell. </summary>
+        /// <param name="vehicle"> The vehicle to add. </param>
+        public void AddVehicle(IVehicle vehicle)
+        {
+            _stackPanel.Children.Add(new ResearchTreeCellVehicleControl(vehicle));
+        }
+    }
+}
