de5b82fd5bd2a40019886358b281d4d35530b6d8
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-22 14:05:54 +0200

    chore: add missing documentation

--- Core.Organization/Objects/SearchSpecifications/Specification.cs
+++ Core.Organization/Objects/SearchSpecifications/Specification.cs
@@ -2,13 +2,23 @@
 
 namespace Core.Organization.Objects.SearchSpecifications
 {
+    /// <summary> A specification used for filtering preferred items from collections before randomizing the former. </summary>
     public class Specification
     {
+        /// <summary> The game mode. </summary>
         public EGameMode GameMode { get; }
+        /// <summary> The nation. </summary>
         public ENation Nation { get; }
+        /// <summary> The branch. </summary>
         public EBranch Branch { get; }
+        /// <summary> The battle rating. </summary>
         public decimal BattleRating { get; }
 
+        /// <summary> Creates a new filter specification with the given parameters. </summary>
+        /// <param name="gameMode"> The game mode. </param>
+        /// <param name="nation"> The nation. </param>
+        /// <param name="branch"> The branch. </param>
+        /// <param name="battleRating"> The battle rating. </param>
         public Specification(EGameMode gameMode, ENation nation, EBranch branch, decimal battleRating)
         {
             GameMode = gameMode;
