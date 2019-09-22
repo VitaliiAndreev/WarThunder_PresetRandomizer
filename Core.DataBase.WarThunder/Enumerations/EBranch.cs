0a40f86714235eadb57a73e632ffe41ef1131480
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-22 19:22:52 +0200

    feature: make EBranch into a set of flags

--- Core.DataBase.WarThunder/Enumerations/EBranch.cs
+++ Core.DataBase.WarThunder/Enumerations/EBranch.cs
@@ -1,12 +1,15 @@
-﻿namespace Core.DataBase.WarThunder.Enumerations
+﻿using System;
+
+namespace Core.DataBase.WarThunder.Enumerations
 {
     /// <summary> Military branches available in War Thunder. </summary>
+    [Flags]
     public enum EBranch
     {
-        None,
-        Army,
-        Helicopters,
-        Aviation,
-        Fleet,
+        None = 0,
+        Army = 1,
+        Helicopters = 2,
+        Aviation = 4,
+        Fleet = 8,
     }
 }
