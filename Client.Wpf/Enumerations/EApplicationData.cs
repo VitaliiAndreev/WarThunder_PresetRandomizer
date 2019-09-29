35f265e3197f807e6d509b15a98bafeba286331c
Vitaly Andreyev <coverthelirush@gmail.com>
2019-09-29 20:38:46 +0200

    chore: update the version number, clean up namespaces

--- Client.Wpf/Enumerations/EApplicationData.cs
+++ Client.Wpf/Enumerations/EApplicationData.cs
@@ -5,7 +5,7 @@ namespace Client.Wpf.Enumerations
     /// <summary> Pre-determined reference information. </summary>
     public class EApplicationData
     {
-        public static Version Version = new Version(0, 2, 0, 0);
+        public static Version Version = new Version(0, 2, 0, 1);
         public const string DevelopmentStageLocalizationKey = ELocalizationKey.Alpha;
 
         public const string ContributionsByVitalyAndreyev = "700+";
