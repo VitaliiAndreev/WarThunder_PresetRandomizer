f0ed3fcf957f09930e83e714c7a762f6169a1182
Vitalii Andreev <coverthelirush@gmail.com>
2019-09-29 14:00:15 +0200

    feature: implement GenericExtensions.IsKeyIn<T, U>()

--- Core/Extensions/GenericExtensions.cs
+++ Core/Extensions/GenericExtensions.cs
@@ -13,5 +13,14 @@ namespace Core.Extensions
         /// <returns></returns>
         public static bool IsIn<T>(this T source, IEnumerable<T> collection) =>
             collection.Contains(source);
+
+        /// <summary> Fluently determines whether the given dictionary contains the object as a key. </summary>
+        /// <typeparam name="T"> The object type. </typeparam>
+        /// <typeparam name="U"> The dictionary value type. </typeparam>
+        /// <param name="source"> The source object. </param>
+        /// <param name="collection"> The dictionary to check. </param>
+        /// <returns></returns>
+        public static bool IsKeyIn<T, U>(this T source, IDictionary<T, U> collection) =>
+            collection.ContainsKey(source);
     }
 }
