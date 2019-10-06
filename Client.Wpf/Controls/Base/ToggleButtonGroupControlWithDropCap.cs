7961b6a76051d9d6f6b98e0bf881c974c3a0ce4e
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-06 19:00:10 +0200

    feature: implement ToggleButtonGroupControlWithDropCap

---
+++ Client.Wpf/Controls/Base/ToggleButtonGroupControlWithDropCap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Client.Wpf.Controls.Base
+{
+    /// <summary> A user control consisting of a row or a column of <see cref="DropCapToggleButton"/>s, indexed by <typeparamref name="T"/> values, and having a tooltip. </summary>
+    /// <typeparam name="T"> The key type. </typeparam>
+    public class ToggleButtonGroupControlWithDropCap<T> : ToggleButtonGroupControl<T>
+    {
+        #region Properties
+
+        /// <summary> The map of the game mode enumeration onto corresponding drop cap toggle buttons. </summary>
+        protected readonly IDictionary<T, DropCapToggleButton> _dropCapToggleButtons;
+
+        #endregion Properties
+        #region Constructors
+
+        /// <summary> Creates a new control. </summary>
+        public ToggleButtonGroupControlWithDropCap()
+        {
+            _dropCapToggleButtons = new Dictionary<T, DropCapToggleButton>();
+        }
+
+        #endregion Constructors
+
+        /// <summary> Applies localization to visible text on the control. </summary>
+        public override void Localize()
+        {
+            base.Localize();
+
+            foreach (var dropCapToggleButton in _dropCapToggleButtons.Values)
+                if (dropCapToggleButton.Tag is T key)
+                    dropCapToggleButton.Caption = ApplicationHelpers.LocalizationManager.GetLocalizedString(key.ToString());
+        }
+    }
+}
