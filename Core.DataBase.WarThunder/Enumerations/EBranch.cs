d739d5b63ec863a910e5a2bdbecb8a4b4d527f1e
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-08 23:55:31 +0200

    chore: remove bit flags from EBranch

--- Core.DataBase.WarThunder/Enumerations/EBranch.cs
+++ Core.DataBase.WarThunder/Enumerations/EBranch.cs
@@ -3,13 +3,12 @@
 namespace Core.DataBase.WarThunder.Enumerations
 {
     /// <summary> Military branches available in War Thunder. </summary>
-    [Flags]
     public enum EBranch
     {
         None = 0,
         Army = 1,
         Helicopters = 2,
-        Aviation = 4,
-        Fleet = 8,
+        Aviation = 3,
+        Fleet = 4,
     }
 }
