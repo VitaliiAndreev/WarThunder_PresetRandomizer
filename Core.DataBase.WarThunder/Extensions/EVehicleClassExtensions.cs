828780081942587291e3816b7e62816020eea9bf
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-09 00:24:35 +0200

    feature: implement EVehicleClassExtensions.GetBranch()

---
+++ Core.DataBase.WarThunder/Extensions/EVehicleClassExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Core.DataBase.WarThunder.Enumerations;
+using Core.Enumerations;
+using Core.Extensions;
+
+namespace Core.DataBase.WarThunder.Extensions
+{
+    /// <summary> Methods extending the <see cref="EVehicleClass"/> enumeration. </summary>
+    public static class EVehicleClassExtensions
+    {
+        /// <summary> Returns the vehicle branch to which the class belongs to. </summary>
+        /// <param name="vehicleClass"> The vehicle class whose branch to get. </param>
+        /// <returns></returns>
+        public static EBranch GetBranch(this EVehicleClass vehicleClass) =>
+            vehicleClass
+                .CastTo<int>()
+                .Do(value => value / EInteger.Number.Ten)
+                .CastTo<EBranch>()
+            ;
+    }
+}
