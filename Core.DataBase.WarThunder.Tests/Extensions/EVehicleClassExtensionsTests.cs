202a2375d37646f6e459b4c0f282f085c5901154
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-09 00:24:50 +0200

    test: add 1 UT for EVehicleClassExtensions.GetBranch()

---
+++ Core.DataBase.WarThunder.Tests/Extensions/EVehicleClassExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Core.DataBase.WarThunder.Enumerations;
+using Core.DataBase.WarThunder.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.DataBase.WarThunder.Tests.Extensions
+{
+    /// <summary> See <see cref="EVehicleClassExtensions"/>. </summary>
+    [TestClass]
+    public class EVehicleClassExtensionsTests
+    {
+        #region Tests: GetBranch()
+
+        [TestMethod]
+        public void GetBranch()
+        {
+            EVehicleClass.None.GetBranch().Should().Be(EBranch.None);
+
+            EVehicleClass.LightTank.GetBranch().Should().Be(EBranch.Army);
+            EVehicleClass.MediumTank.GetBranch().Should().Be(EBranch.Army);
+            EVehicleClass.HeavyTank.GetBranch().Should().Be(EBranch.Army);
+
+            EVehicleClass.AttackHelicopter.GetBranch().Should().Be(EBranch.Helicopters);
+            EVehicleClass.UtilityHelicopter.GetBranch().Should().Be(EBranch.Helicopters);
+
+            EVehicleClass.Fighter.GetBranch().Should().Be(EBranch.Aviation);
+            EVehicleClass.Attacker.GetBranch().Should().Be(EBranch.Aviation);
+            EVehicleClass.Bomber.GetBranch().Should().Be(EBranch.Aviation);
+
+            EVehicleClass.Boat.GetBranch().Should().Be(EBranch.Fleet);
+            EVehicleClass.HeavyBoat.GetBranch().Should().Be(EBranch.Fleet);
+            EVehicleClass.Barge.GetBranch().Should().Be(EBranch.Fleet);
+            EVehicleClass.Destroyer.GetBranch().Should().Be(EBranch.Fleet);
+            EVehicleClass.LightCruiser.GetBranch().Should().Be(EBranch.Fleet);
+            EVehicleClass.HeavyCruiser.GetBranch().Should().Be(EBranch.Fleet);
+        }
+
+        #endregion Tests: GetBranch()
+    }
+}
