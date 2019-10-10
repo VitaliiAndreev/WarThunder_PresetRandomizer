7b1e9d4086c94d2ed2d5d981c8c2cad37742acb2
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-10 22:23:53 +0200

    feature: implement EBranchExtensions.GetVehicleClasses()

---
+++ Core.DataBase.WarThunder/Extensions/EBranchExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Core.DataBase.WarThunder.Enumerations;
+using Core.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.DataBase.WarThunder.Extensions
+{
+    /// <summary> Methods extending the <see cref="EBranch"/> enumeration. </summary>
+    public static class EBranchExtensions
+    {
+        /// <summary> Returns vehicle classes which belong to the branch. </summary>
+        /// <param name="branch"> The branch whose vehicle classes to get. </param>
+        /// <returns></returns>
+        public static IEnumerable<EVehicleClass> GetVehicleClasses(this EBranch branch) =>
+            typeof(EVehicleClass)
+                .GetEnumerationItems<EVehicleClass>()
+                .Where(vehicleClass => vehicleClass.GetBranch() == branch)
+            ;
+    }
+}
