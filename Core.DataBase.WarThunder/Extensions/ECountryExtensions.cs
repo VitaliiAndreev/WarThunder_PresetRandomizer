868c42b87b7eaf6d8a4bbd40e1398dfcbc35f4a7
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-19 13:13:37 +0200

    feature: implement ECountryExtensions.GetNations()

---
+++ Core.DataBase.WarThunder/Extensions/ECountryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataBase.WarThunder.Enumerations;
+using System.Collections.Generic;
+
+namespace Core.DataBase.WarThunder.Extensions
+{
+    public static class ECountryExtensions
+    {
+        /// <summary> Returns all nations that have vehicles of the given <paramref name="country"/>. </summary>
+        /// <param name="country"> The country to search by. </param>
+        /// <returns></returns>
+        public static IEnumerable<ENation> GetNations(this ECountry country) =>
+            EReference.NationsByCountry.TryGetValue(country, out var nations)
+                ? nations
+                : new List<ENation>();
+    }
+}
