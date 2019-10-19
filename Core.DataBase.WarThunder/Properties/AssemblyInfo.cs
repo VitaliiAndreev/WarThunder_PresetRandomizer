85e1228b77ab90d3533db062b8ff4f4c1337bed0
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-19 13:14:25 +0200

    chore: add missing assembly directives

--- Core.DataBase.WarThunder/Properties/AssemblyInfo.cs
+++ Core.DataBase.WarThunder/Properties/AssemblyInfo.cs
@@ -36,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Core.DataBase.WarThunder.Tests")]
+[assembly: InternalsVisibleTo("Core.Organization")]
