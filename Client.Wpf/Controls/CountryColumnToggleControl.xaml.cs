f3b55a028396a713fa058a1fa52352172ece05fa
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-20 16:45:16 +0200

    feature: implement CountryColumnToggleControl and CountryToggleControl

---
+++ Client.Wpf/Controls/CountryColumnToggleControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Client.Wpf.Controls.Base;
+using Client.Wpf.Enumerations;
+using Core.DataBase.WarThunder.Enumerations;
+using Core.DataBase.WarThunder.Extensions;
+using Core.DataBase.WarThunder.Objects;
+using Core.Extensions;
+using System.Linq;
+
+namespace Client.Wpf.Controls
+{
+    /// <summary> Interaction logic for CountryColumnToggleControl.xaml. </summary>
+    public partial class CountryColumnToggleControl : ColumnToggleControlWithResources<ENation, NationCountryPair>
+    {
+        #region Fields
+
+        /// <summary> The nation to which countries belong to. </summary>
+        private ENation _nation;
+
+        #endregion Fields
+        #region Properties
+
+        /// <summary> The nation to which countries belong to. </summary>
+        public override ENation Owner
+        {
+            get => _nation;
+            set
+            {
+                _nation = value;
+                _panel.Children.Clear();
+
+                CreateToggleButtons
+                (
+                    _panel,
+                    _groupedItems[value],
+                    _groupedItems
+                        .Values
+                        .SelectMany(nationCountryPairs => nationCountryPairs)
+                        .Where(nationCountryPair => nationCountryPair.Country.IsKeyIn(EReference.CountryIconsKeys))
+                        .ToDictionary(nationCountryPair => nationCountryPair, nationCountryPair => EReference.CountryIconsKeys[nationCountryPair.Country]),
+                    EStyleKey.ToggleButton.CountryToggle, false
+                );
+            }
+        }
+
+        #endregion Properties
+        #region Constructors
+
+        /// <summary> Creates a new control. </summary>
+        /// <param name="branch"> The nation to which countries belong to. </param>
+        public CountryColumnToggleControl(ENation nation)
+            : this()
+        {
+            Owner = nation;
+        }
+
+        /// <summary> Creates a new control. </summary>
+        public CountryColumnToggleControl()
+        {
+            var countriesByNations = typeof(ENation)
+                .GetEnumValues()
+                .Cast<ENation>()
+                .Except(ENation.None)
+                .ToDictionary(nation => nation, nation => nation.GetCountries().Select(country => new NationCountryPair(nation, country)))
+            ;
+
+            _groupedItems.AddRange(countriesByNations);
+            
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ Client.Wpf/Controls/CountryToggleControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Client.Wpf.Controls.Base;
+using Client.Wpf.Extensions;
+using Core.DataBase.WarThunder.Enumerations;
+using Core.DataBase.WarThunder.Objects;
+using Core.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Client.Wpf.Controls
+{
+    /// <summary> Interaction logic for CountryToggleControl.xaml. </summary>
+    public partial class CountryToggleControl : ColumnToggleGroupControl<ENation, CountryColumnToggleControl, NationCountryPair>
+    {
+        #region Constructors
+
+        /// <summary> Creates a new control. </summary>
+        public CountryToggleControl()
+        {
+            var nations = typeof(ENation).GetEnumValues().Cast<ENation>().Except(ENation.None);
+
+            InitializeComponent();
+            CreateToggleColumns(nations);
+        }
+
+        #endregion Constructors
+
+        /// <summary> Creates columns of toggle buttons for given <paramref name="nations"/>, with character icons. </summary>
+        /// <param name="nations"> Nations to create columns of toggle buttons for. </param>
+        public override void CreateToggleColumns(IEnumerable<ENation> nations)
+        {
+            foreach (var nation in nations)
+            {
+                var columnControl = new CountryColumnToggleControl(nation)
+                {
+                    Tag = nation,
+                };
+
+                columnControl.Click += OnClick;
+                columnControl.AddToPanel(_grid, true);
+
+                ToggleClassColumns.Add(nation, columnControl);
+            }
+        }
+
+        /// <summary> Toggles a button corresponding to the specified <paramref name="nationCountryPair"/>. </summary>
+        /// <param name="nationCountryPair"> The country whose button to toggle. </param>
+        /// <param name="newState"> Whether to toggle the button on or off. </param>
+        public override void Toggle(NationCountryPair nationCountryPair, bool newState)
+        {
+            if (ToggleClassColumns.TryGetValue(nationCountryPair.Nation, out var column))
+                column.Toggle(nationCountryPair, newState);
+        }
+    }
+}
