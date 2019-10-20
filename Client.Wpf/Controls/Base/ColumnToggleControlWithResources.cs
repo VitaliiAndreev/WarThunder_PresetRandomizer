4fb39ae2a677f323d90002448d814cd52c712463
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-20 16:38:51 +0200

    feature: implement ToggleButtonGroupControlWithResource<T>, ColumnToggleControlWithResources<T, U>, and ColumnToggleControlWithTooltips<T, U>

---
+++ Client.Wpf/Controls/Base/ColumnToggleControlWithResources.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Client.Wpf.Controls.Base
+{
+    /// <summary> A user control consisting of a column of <see cref="ToggleButton"/>s, indexed by <typeparamref name="T"/> values. </summary>
+    /// <typeparam name="T"> The key type. </typeparam>
+    /// <typeparam name="U"> The owner type. </typeparam>
+    public abstract class ColumnToggleControlWithResources<T, U>: ToggleButtonGroupControlWithResource<U>
+    {
+        #region Fields
+
+        /// <summary> Items grouped by their owner branches. </summary>
+        protected readonly IDictionary<T, IEnumerable<U>> _groupedItems;
+
+        #endregion Fields
+        #region Properties
+
+        /// <summary> The branch to which vehicle classes belong to. </summary>
+        public abstract T Owner { get; set; }
+
+        /// <summary> Enabled vehicle classes. </summary>
+        public IEnumerable<U> EnabledItems { get; }
+
+        #endregion Properties
+        #region Constructors
+
+        /// <summary> Creates a new control. </summary>
+        /// <param name="owner"> The owner for which to create the control. </param>
+        public ColumnToggleControlWithResources(T owner)
+            : this()
+        {
+            Owner = owner;
+        }
+
+        /// <summary> Creates a new control. </summary>
+        public ColumnToggleControlWithResources()
+        {
+            _groupedItems = new Dictionary<T, IEnumerable<U>>();
+
+            EnabledItems = new List<U>();
+        }
+
+        #endregion Constructors
+
+        /// <summary> Creates toggle buttons for given <paramref name="enumerationItems"/>, with character <paramref name="icons"/>. </summary>
+        /// <param name="panel"> The panel to add buttons into. </param>
+        /// <param name="enumerationItems"> Enumeration items to create toggle buttons for. </param>
+        /// <param name="icons"> Icons for <paramref name="enumerationItems"/>. </param>
+        /// <param name="styleKey"> The key of the style (defined in <see cref="WpfClient"/> and referenced by <see cref="EStyleKey"/>) to apply. </param>
+        public void CreateToggleButtons(Panel panel, IEnumerable<U> enumerationItems, IDictionary<U, char> icons, string styleKey) =>
+            CreateToggleButtons(panel, enumerationItems, icons, styleKey, false);
+    }
+}
---
+++ Client.Wpf/Controls/Base/ToggleButtonGroupControlWithResource.cs
@@ -0,0 +1,43 @@
+﻿using Client.Wpf.Enumerations;
+using Client.Wpf.Extensions;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Client.Wpf.Controls.Base
+{
+    /// <summary> A user control consisting of a row or a column of <see cref="DropCapToggleButton"/>s, indexed by <typeparamref name="T"/> values, and having content read from resources. </summary>
+    /// <typeparam name="T"> The key type. </typeparam>
+    public class ToggleButtonGroupControlWithResource<T> : ToggleButtonGroupControlWithToolTip<T>
+    {
+        /// <summary> Creates toggle buttons for given <paramref name="enumerationItems"/>, with icons read from resources by their <paramref name="resourceKeys"/>. </summary>
+        /// <param name="panel"> The panel to add buttons into. </param>
+        /// <param name="enumerationItems"> Enumeration items to create toggle buttons for. </param>
+        /// <param name="resourceKeys"> Resource keys for <paramref name="enumerationItems"/>. </param>
+        /// <param name="styleKey"> The key of the style (defined in <see cref="WpfClient"/> and referenced by <see cref="EStyleKey"/>) to apply. </param>
+        /// <param name="horizontal"> Whether to arrange buttons in a row or in a column. </param>
+        public void CreateToggleButtons(Panel panel, IEnumerable<T> enumerationItems, IDictionary<T, string> resourceKeys, string styleKey, bool horizontal = true)
+        {
+            foreach (var enumerationItem in enumerationItems)
+            {
+                var toggleButton = new ToggleButton
+                {
+                    Style = this.GetStyle(styleKey),
+                    Tag = enumerationItem,
+                    Content = new Image()
+                    {
+                        Source = Application.Current.MainWindow.FindResource(resourceKeys[enumerationItem]) as ImageSource,
+                        Style = this.GetStyle(EStyleKey.Image.FlagIcon),
+                    },
+                };
+
+                toggleButton.Click += OnClick;
+                toggleButton.AddToPanel(panel, horizontal);
+
+                Buttons.Add(enumerationItem, toggleButton);
+            }
+        }
+    }
+}
