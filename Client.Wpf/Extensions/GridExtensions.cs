cabf4d6ed46344fc8d07e793f6577eb002abb34d
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-26 22:58:24 +0200

    feature: implement GridExtensions.Remove()

---
+++ Client.Wpf/Extensions/GridExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.Wpf.Extensions
+{
+    /// <summary> Methods extending the <see cref="Grid"/> class. </summary>
+    public static class GridExtensions
+    {
+        /// <summary> Removes the given <paramref name="element"/> from the <paramref name="grid"/> along with its row/column (if not other elements are present). </summary>
+        /// <param name="grid"> The grid to remove the <paramref name="element"/> from. </param>
+        /// <param name="element"> The element to remove from the <paramref name="grid"/>. </param>
+        /// <param name="column"> Whether to remove </param>
+        public static void Remove(this Grid grid, UIElement element, bool column = true)
+        {
+            grid.Children.Remove(element);
+
+            var children = grid.Children.OfType<UIElement>();
+
+            if (column)
+            {
+                var columnIndex = Grid.GetColumn(element);
+
+                if (!children.Any(item => Grid.GetColumn(item) == columnIndex))
+                    grid.ColumnDefinitions.RemoveAt(columnIndex);
+            }
+            else
+            {
+                var rowIndex = Grid.GetRow(element);
+
+                if (!children.Any(item => Grid.GetRow(item) == rowIndex))
+                    grid.RowDefinitions.RemoveAt(rowIndex);
+            }
+        }
+    }
+}
