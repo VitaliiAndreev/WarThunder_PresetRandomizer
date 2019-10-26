216f26c151fa73ab7a1cc4703d88295a56082e41
Vitaly Andreyev <coverthelirush@gmail.com>
2019-10-26 22:58:56 +0200

    feature: implement IDictionaryExtensions.Has()

--- Core.Organization/Extensions/IDictionaryExtensions.cs
+++ Core.Organization/Extensions/IDictionaryExtensions.cs
@@ -1,12 +1,22 @@
-﻿using Core.DataBase.WarThunder.Objects.Interfaces;
+﻿using Core.DataBase.WarThunder.Enumerations;
+using Core.DataBase.WarThunder.Objects.Interfaces;
 using Core.Organization.Helpers.Interfaces;
+using Core.Organization.Objects;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Organization.Extensions
 {
     /// <summary> Methods extending the <see cref="IDictionary{TKey, TValue}"/> interface. </summary>
     public static class IDictionaryExtensions
     {
+        /// <summary> Checks whether a research tree for the given <paramref name="nation"/> exists. </summary>
+        /// <param name="dictionary"> The dictionary of research trees indexed by nations. </param>
+        /// <param name="nation"> The nation to check. </param>
+        /// <returns></returns>
+        public static bool Has(this IDictionary<ENation, ResearchTree> dictionary, ENation nation) =>
+            dictionary.TryGetValue(nation, out var researchTree) && researchTree is ResearchTree && researchTree.Any();
+
         /// <summary>
         /// Randomly picks several vehicles with the highest battle rating from the specified dictionary.
         /// If there are fewer vehicles with the highest battle rating than required, vehicles with the next lower battle rating step are rendomly taken, and so on.
