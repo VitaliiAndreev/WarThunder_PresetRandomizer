a0e0d36a8520a98087a05430315b4bdc95ab6bed
Vitaly Andreyev <coverthelirush@gmail.com>
2019-11-04 17:45:30 +0200

    feature: implement MemoryStreamExtensions.Read() as an overload and .OutputToConsole()

---
+++ Core/Extensions/MemoryStreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Core.Extensions
+{
+    /// <summary> Methods extending the <see cref="MemoryStream"/> class. </summary>
+    public static class MemoryStreamExtensions
+    {
+        /// <summary> Reads all characters from the current position to the end of the <paramref name="memoryStream"/>. </summary>
+        /// <param name="memoryStream"> The memory stream to read. </param>
+        /// <returns></returns>
+        public static string Read(this MemoryStream memoryStream)
+        {
+            using (var streamReader = new StreamReader(memoryStream))
+                return streamReader.ReadToEnd();
+        }
+
+        /// <summary> Reads all characters from the current position to the end of the <paramref name="memoryStream"/> and prints them to <see cref="Console"/>. </summary>
+        /// <param name="memoryStream"> The memory stream to output. </param>
+        public static void OutputToConsole(this MemoryStream memoryStream) =>
+            Console.WriteLine(memoryStream.Read());
+    }
+}
