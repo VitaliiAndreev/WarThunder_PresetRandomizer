c5ba30770a0d285bf609bb00f4a80ca053be981c
Vitaly Andreyev <coverthelirush@gmail.com>
2019-12-17 22:01:22 +0200

    chore: update the version number, add new entities to the default configuration

--- Client.Wpf/Enumerations/EApplicationData.cs
+++ Client.Wpf/Enumerations/EApplicationData.cs
@@ -5,7 +5,7 @@ namespace Client.Wpf.Enumerations
     /// <summary> Pre-determined reference information. </summary>
     public class EApplicationData
     {
-        public static Version Version = new Version(0, 2, 5, 0);
+        public static Version Version = new Version(0, 2, 5, 1);
         public const string DevelopmentStageLocalizationKey = ELocalizationKey.Alpha;
 
         public const string ContributionsByVitalyAndreyev = "900+";
