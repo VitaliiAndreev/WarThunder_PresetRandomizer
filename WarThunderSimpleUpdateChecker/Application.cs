62d649fc9485840ebb2a676ff50d27d3e9406c5a
Vitaly Andreyev <coverthelirush@gmail.com>
2019-12-29 13:14:25 +0200

    chore: include mission data files back into the unpack list of the Patch Logger

--- WarThunderSimpleUpdateChecker/Application.cs
+++ WarThunderSimpleUpdateChecker/Application.cs
@@ -91,7 +91,6 @@ namespace WarThunderSimpleUpdateChecker
             var excludedBinFileNames = new string[]
             {
                 EFile.WarThunder.GuiParameters,
-                EFile.WarThunder.MissionParameters,
                 EFile.WarThunder.WebUiParameters,
             };
 
