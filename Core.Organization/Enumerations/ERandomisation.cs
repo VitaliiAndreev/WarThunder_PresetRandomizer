648900117d43790f7bc24e3777b585c3ad64a5fb
Vitaly Andreyev <coverthelirush@gmail.com>
2020-01-03 21:20:52 +0200

    chore: correct documentation

--- Core.Organization/Enumerations/ERandomisation.cs
+++ Core.Organization/Enumerations/ERandomisation.cs
@@ -3,9 +3,9 @@
     /// <summary> Methods of randomisation. </summary>
     public enum ERandomisation
     {
-        /// <summary> Random selection is done in every category before selecting any vehicles that adhere to the criteria. </summary>
+        /// <summary> Random selection is done in every major category (in predetermined order) before selecting any vehicles that adhere to the criteria. </summary>
         CategoryBased,
-        /// <summary> Filter criteria don't determine selecting but only what vehicles are available. Randomisation is done only over the set of available vehicles. </summary>
+        /// <summary> Filter criteria don't determine selection but only what vehicles are available. Randomisation is done only over the set of available vehicles. </summary>
         VehicleBased,
     }
 }
