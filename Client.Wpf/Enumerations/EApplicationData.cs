1957211bf5b69cd09c83a61aad0dcc1e31cf36e4
Vitaly Andreyev <coverthelirush@gmail.com>
2020-02-15 13:25:21 +0200

    chore: update the version number, add new vehicles to the default filter

--- Client.Wpf/Enumerations/EApplicationData.cs
+++ Client.Wpf/Enumerations/EApplicationData.cs
@@ -5,7 +5,7 @@ namespace Client.Wpf.Enumerations
     /// <summary> Pre-determined reference information. </summary>
     public class EApplicationData
     {
-        public static Version Version = new Version(0, 2, 5, 1);
+        public static Version Version = new Version(0, 2, 6, 0);
         public const string DevelopmentStageLocalizationKey = ELocalizationKey.Alpha;
 
         public const string ContributionsByVitalyAndreyev = "900+";
