2bd822bffefbb15ea956b9a9556ffd95f98f6e5f
Vitaly Andreyev <coverthelirush@gmail.com>
2020-04-14 21:52:33 +0200

    chore: update the version number

--- Client.Wpf/Enumerations/EApplicationData.cs
+++ Client.Wpf/Enumerations/EApplicationData.cs
@@ -5,7 +5,7 @@ namespace Client.Wpf.Enumerations
     /// <summary> Pre-determined reference information. </summary>
     public class EApplicationData
     {
-        public static Version Version = new Version(0, 2, 6, 1);
+        public static Version Version = new Version(0, 2, 6, 2);
         public const string DevelopmentStageLocalizationKey = ELocalizationKey.Alpha;
 
         public const string ContributionsByVitalyAndreyev = "1000+";
