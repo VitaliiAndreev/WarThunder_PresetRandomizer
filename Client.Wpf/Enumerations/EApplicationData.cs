f76bed7af8a97a482cba046c5e6659c8d1f8da8c
Vitaly Andreyev <coverthelirush@gmail.com>
2020-04-17 02:06:40 +0200

    refactor: centralise the version number in Core\Properties\VersionInfo.cs

--- Client.Wpf/Enumerations/EApplicationData.cs
+++ Client.Wpf/Enumerations/EApplicationData.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Core.Enumerations;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 
 namespace Client.Wpf.Enumerations
 {
     /// <summary> Pre-determined reference information. </summary>
     public class EApplicationData
     {
-        public static Version Version = new Version(0, 2, 6, 1);
-        public const string DevelopmentStageLocalizationKey = ELocalizationKey.Alpha;
-
-        public const string ContributionsByVitalyAndreyev = "1000+";
+        public static Version Version;
+        public static string DevelopmentStageLocalizationKey;
 
         public const string LinkToYouTubePlaylist = "https://www.youtube.com/playlist?list=PLTkOsj0Uogp4z4Px8IrmZIl_z6M60mmqX";
+        public const string ContributionsByVitalyAndreyev = "1000+";
+
+        static EApplicationData()
+        {
+            var productVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            var versionParts = productVersion.Split(ECharacter.Space);
+
+            Version = new Version(versionParts.First());
+            DevelopmentStageLocalizationKey = versionParts.Last();
+        }
     }
 }
---
+++ Core/Properties/VersionInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.6.1")]
+[assembly: AssemblyFileVersion("0.2.6.1")]
+[assembly: AssemblyInformationalVersion("0.2.6.1 Alpha")] // The current format is the full version number and the development stage separated by space.
