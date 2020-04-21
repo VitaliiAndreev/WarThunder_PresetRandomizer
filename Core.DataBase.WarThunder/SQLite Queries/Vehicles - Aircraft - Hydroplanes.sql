63d3e29311fc436b9f7355a64fa96e64ca82a379
Vitaly Andreyev <coverthelirush@gmail.com>
2020-04-21 21:30:37 +0200

    chore: add new SQLite queries

---
+++ Core.DataBase.WarThunder/SQLite Queries/Vehicles - Aircraft - Hydroplanes.sql
@@ -0,0 +1,38 @@
+select
+	substr(nation.GaijinId, 9) [Nation]
+	,case lower(vehicle.Country)
+		when lower(substr(nation.GaijinId, 9)) then ''
+		else vehicle.Country
+	end [Country]
+	,fullName.English [Name]
+	,vehicle.GaijinId [Gaijin ID]
+	,vehicle.Rank [Rank]
+	,vehicle.Class [Class]
+	,case subclass.First
+		when 'None' then ''
+		when vehicle.Class then ''
+		else subclass.First
+	end [Subclass]
+	,vehicle.IsHiddenUnlessOwned [Hidden]
+	,vehicle.IsPremium [Premium]
+	,vehicle.IsPurchasableForGoldenEagles [GE]
+	,vehicle.IsSquadronVehicle [Quadron]
+	,vehicle.IsSoldInTheStore [Store]
+	,vehicle.IsSoldOnTheMarket [Market]
+	,vehicle.IsAvailableOnlyOnConsoles [Consoles Only]
+from
+	objVehicles vehicle
+	join locVehicles_FullName fullName on fullName.objVehicles_Id = vehicle.Id
+	join objNations nation on nation.Id = vehicle.objNations_Id
+	join objVehicles_SubClass subclass on subclass.objVehicles_Id = vehicle.Id
+	left join objAircraftTags airTagSet on airTagSet.objVehicles_Id = vehicle.Id
+where
+	airTagSet.IsHydroplane
+	and vehicle.GaijinId not like '%_football'
+	and vehicle.GaijinId not like '%_nw'
+	and vehicle.GaijinId not like '%_race'
+	and vehicle.GaijinId not like '%_space_suit'
+	and vehicle.GaijinId not like '%_tutorial'
+	and vehicle.GaijinId not like 'uk_centaur_aa_mk_%'
+order by
+	[Nation], [Country], [Class], [Subclass], [Name]
---
+++ Core.DataBase.WarThunder/SQLite Queries/Vehicles - Ground - Scout Vehicles.sql
@@ -0,0 +1,39 @@
+select
+	substr(nation.GaijinId, 9) [Nation]
+	,case lower(vehicle.Country)
+		when lower(substr(nation.GaijinId, 9)) then ''
+		else vehicle.Country
+	end [Country]
+	,substr(branch.GaijinId, length(substr(nation.GaijinId, 9)) + 2) [Branch]
+	,fullName.English [Name]
+	,vehicle.GaijinId [Gaijin ID]
+	,vehicle.Rank [Rank]
+	,vehicle.Class [Class]
+	,case subclass.First
+		when 'None' then ''
+		when vehicle.Class then ''
+		else subclass.First
+	end [Subclass]
+	,vehicle.IsHiddenUnlessOwned [Hidden]
+	,vehicle.IsPremium [Premium]
+	,vehicle.IsPurchasableForGoldenEagles [GE]
+	,vehicle.IsSoldInTheStore [Store]
+	,vehicle.IsSoldOnTheMarket [Market]
+	,vehicle.IsAvailableOnlyOnConsoles [Consoles Only]
+from
+	objVehicles vehicle
+	join locVehicles_FullName fullName on fullName.objVehicles_Id = vehicle.Id
+	join objBranches branch on branch.Id = vehicle.objBranches_Id
+	join objNations nation on nation.Id = vehicle.objNations_Id
+	join objVehicles_SubClass subclass on subclass.objVehicles_Id = vehicle.Id
+	left join objGroundVehicleTags groundTagSet on groundTagSet.objVehicles_Id = vehicle.Id
+where
+	groundTagSet.CanScout
+	and vehicle.GaijinId not like '%_football'
+	and vehicle.GaijinId not like '%_nw'
+	and vehicle.GaijinId not like '%_race'
+	and vehicle.GaijinId not like '%_space_suit'
+	and vehicle.GaijinId not like '%_tutorial'
+	and vehicle.GaijinId not like 'uk_centaur_aa_mk_%'
+order by
+	[Nation], [Country], [Branch], [Class], [Subclass], [Name]
