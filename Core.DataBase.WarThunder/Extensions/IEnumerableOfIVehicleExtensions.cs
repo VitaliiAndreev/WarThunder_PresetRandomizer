cad5141a7fda8b73f39dd67573415a8cd51c210d
Vitaly Andreyev <coverthelirush@gmail.com>
2020-05-04 22:46:29 +0200

    refactor: reduce code duplicity, use extension methods

---
+++ Core.DataBase.WarThunder/Extensions/IEnumerableOfIVehicleExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Core.DataBase.WarThunder.Objects.Interfaces;
+using Core.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.DataBase.WarThunder.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        #region Sorting
+
+        public static IOrderedEnumerable<IVehicle> OrderByNationSubclassRankId(this IEnumerable<IVehicle> vehicles)
+        {
+            return vehicles
+                .OrderBy(vehicle => vehicle.Nation.AsEnumerationItem)
+                .ThenBySubclassRankId()
+            ;
+        }
+
+        public static IOrderedEnumerable<IVehicle> OrderByClassSubclassRankId(this IEnumerable<IVehicle> vehicles)
+        {
+            return vehicles
+                .AsOrderedEnumerable()
+                .ThenByClassSubclassRankId()
+            ;
+        }
+
+        public static IOrderedEnumerable<IVehicle> OrderBySubclassRankId(this IEnumerable<IVehicle> vehicles)
+        {
+            return vehicles
+                .AsOrderedEnumerable()
+                .ThenBySubclassRankId();
+        }
+
+        #endregion Sorting
+    }
+}
