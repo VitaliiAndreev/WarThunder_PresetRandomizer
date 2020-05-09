0c545ceee8a12c797f5827331f8d37306b133741
Vitaly Andreyev <coverthelirush@gmail.com>
2020-05-09 16:14:11 +0200

    feature: add research cost to vehicle lists, rework lite object implementation

---
+++ Client.Core/Enumerations/LiteObjectProfiles/EVehicleProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Client.Shared.LiteObjectProfiles
+{
+    [Flags]
+    public enum EVehicleProfile
+    {
+        None = 0,
+        Full = 1,
+        Nation = 2,
+        NationAndCountry = 4,
+        Country = 8,
+        NationAndBranch = 16,
+        Branch = 32,
+        BranchAndCountry = 64,
+        BranchAndClass = 128,
+        Class = 256,
+        NationAndClass = 512,
+        Tag = 1024,
+        NationAndTag = 2048,
+        Subclass = 4096,
+        NationAndSubclass = 8192,
+    }
+}
