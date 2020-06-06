9f229174cc17bc1a0c25aac224374e53205201fc
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-06 19:38:13 +0200

    test: add 1 UT for HtmlParser.GetHtmlDocumentNode()

---
+++ Core.Web.Tests/Helpers/HtmlParserTests.cs
@@ -0,0 +1,25 @@
+﻿using Core.Tests;
+using Core.Web.Helpers;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Web.Tests.Helpers
+{
+    [TestClass]
+    public class HtmlParserTests
+    {
+        [TestMethod]
+        public void GetHtmlDocumentNode()
+        {
+            // arrange
+            var parser = new HtmlParser(Presets.Logger);
+
+            // act
+            var result = parser.GetHtmlDocumentNode("https://google.com");
+
+            // assert
+            result.Should().NotBeNull();
+            result.ChildNodes.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ Core.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("870e6e4f-6bff-4e0c-b22d-619703980023")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
