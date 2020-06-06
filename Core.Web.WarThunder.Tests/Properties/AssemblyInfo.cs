af6ff0c150f113aa418ee4530d4384ca48ad6ef0
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-06 19:39:42 +0200

    test: add 1 UT for ThunderSkillParser.GetVehicleUsage()

---
+++ Core.Web.WarThunder.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core.Web.WarThunder.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core.Web.WarThunder.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b56cf8a-b3f5-4c1e-be56-5f8f42d628bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
