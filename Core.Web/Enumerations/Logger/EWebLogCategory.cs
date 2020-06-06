e59a7f6617e2783a6d71a067edbe632aad611e49
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-06 19:33:30 +0200

    feature: create Core.Web, implement (I)HtmlParser and HtmlNodeExtensions

---
+++ Core.Web/Enumerations/Logger/EWebLogCategory.cs
@@ -0,0 +1,10 @@
+﻿using Core.Enumerations;
+using Core.Enumerations.Logger;
+
+namespace Core.Web.Enumerations.Logger
+{
+    public class EWebLogCategory : ECoreLogCategory
+    {
+        public static readonly string HtmlParser = $"{EWord.Html} {EWord.Parser}";
+    }
+}
