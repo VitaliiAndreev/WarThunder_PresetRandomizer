0b24459aca90ca04bb40dd7509fc959d7c498dcb
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-07 18:12:21 +0200

    test: add 6 UTs for ColorExtensions.InterpolateTo()

---
+++ Client.Shared.Wpf.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Client.Shared.Wpf.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Client.Shared.Wpf.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d454e08b-77d2-4af7-a15d-0fd86a354c0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
