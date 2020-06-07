70c8c5fd3082f8ef50ac8e0394294f49c80eaee6
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-07 17:28:52 +0200

    feature: implement ByteExtensions.InterpolateTo()

---
+++ Core/Extensions/ByteExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Core.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Extensions
+{
+    public static class ByteExtensions
+    {
+        /// <summary>
+        /// Calculates the <paramref name="stepCount"/> amount of points between the <paramref name="start"/> and the <paramref name="end"/>.
+        /// Start and end points are not included in the output.
+        /// </summary>
+        /// <param name="start"> The starting point. </param>
+        /// <param name="end"> The endpoint. </param>
+        /// <param name="stepCount"> The amount of steps to interpolate. </param>
+        /// <returns></returns>
+        public static IEnumerable<byte> InterpolateTo(this byte start, byte end, int stepCount)
+        {
+            if (start == end)
+                return Enumerable.Range(EInteger.Number.Zero, stepCount).Select(number => start);
+
+            var reversed = start > end;
+            var steps = new List<byte>();
+
+            var actualStart = reversed ? end : start;
+            var actualEnd = reversed ? start : end;
+
+            for (var stepNumber = EInteger.Number.One; stepNumber <= stepCount; stepNumber++)
+            {
+                var step = Convert.ToByte(actualStart + ((actualEnd - actualStart) * stepNumber / (stepCount + EInteger.Number.One)));
+                steps.Add(step);
+            }
+
+            if (reversed)
+                steps.Reverse();
+
+            return steps;
+        }
+    }
+}
