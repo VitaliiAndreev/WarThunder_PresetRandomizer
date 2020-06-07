17f04fb6dcce5feaedbeaa10d6f78a1daa445c4e
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-08 01:52:33 +0200

    refactor: implement a retry mechanic in HtmlParser.GetHtmlDocumentNode()

--- Core.Web/Helpers/HtmlParser.cs
+++ Core.Web/Helpers/HtmlParser.cs
@@ -1,15 +1,23 @@
-﻿using Core.Enumerations.Logger;
+﻿using Core.Enumerations;
+using Core.Enumerations.Logger;
 using Core.Extensions;
 using Core.Helpers.Logger;
 using Core.Helpers.Logger.Interfaces;
 using Core.Web.Enumerations.Logger;
 using Core.Web.Helpers.Interfaces;
 using HtmlAgilityPack;
+using System;
+using System.Threading;
 
 namespace Core.Web.Helpers
 {
     public class HtmlParser : LoggerFluency, IHtmlParser
     {
+        #region Constants
+
+        private const int _retryDelay = EInteger.Time.MillisecondsInSecond;
+
+        #endregion Constants
         #region Constructors
 
         public HtmlParser(params IConfiguredLogger[] loggers)
@@ -20,10 +28,23 @@ namespace Core.Web.Helpers
 
         #endregion Constructors
 
-        public HtmlNode GetHtmlDocumentNode(string url) =>
-            new HtmlWeb()
-                .Load(url)
-                .DocumentNode
-            ;
+        public HtmlNode GetHtmlDocumentNode(string url, int retryAttempts = EInteger.Number.One, Exception internalException = null)
+        {
+            if (retryAttempts.IsZero())
+                throw new TimeoutException(EWebLogMessage.FailedToRead.FormatFluently(url), internalException);
+
+            try
+            {
+                return new HtmlWeb()
+                    .Load(url)
+                    .DocumentNode
+                ;
+            }
+            catch (Exception exception)
+            {
+                Thread.Sleep(_retryDelay);
+                return GetHtmlDocumentNode(url, --retryAttempts, exception);
+            }
+        }
     }
 }
