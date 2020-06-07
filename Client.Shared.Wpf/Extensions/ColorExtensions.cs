62152ce68932f08ec77cd8d874ff330ef1298311
Vitaly Andreyev <coverthelirush@gmail.com>
2020-06-07 18:09:53 +0200

    feature: implement ColorExtensions.From() and its overloads

--- Client.Shared.Wpf/Extensions/ColorExtensions.cs
+++ Client.Shared.Wpf/Extensions/ColorExtensions.cs
@@ -8,5 +8,19 @@ namespace Client.Shared.Wpf.Extensions
 {
     public static class ColorExtensions
     {
+        #region Methods: From()
+
+        public static Color From(this Color color, byte allColors) => color.From(allColors, allColors, allColors);
+
+        public static Color From(this Color color, byte red, byte green, byte blue)
+        {
+            color.R = red;
+            color.G = green;
+            color.B = blue;
+
+            return color;
+        }
+
+        #endregion Methods: From()
     }
 }
