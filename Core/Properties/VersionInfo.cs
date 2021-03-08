8cfac6dca1345301aa21022ff6a91056fefbfa07
Vitaly Andreyev <coverthelirush@gmail.com>
2021-03-08 22:14:03 +0200

    chore: update the version number

--- Core/Properties/VersionInfo.cs
+++ Core/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.2.2")]
-[assembly: AssemblyFileVersion("0.3.2.2")]
-[assembly: AssemblyInformationalVersion("0.3.2.2 Beta")] // The current format is the full version number and the development stage separated by space.
+[assembly: AssemblyVersion("0.3.2.3")]
+[assembly: AssemblyFileVersion("0.3.2.3")]
+[assembly: AssemblyInformationalVersion("0.3.2.3 Beta")] // The current format is the full version number and the development stage separated by space.
