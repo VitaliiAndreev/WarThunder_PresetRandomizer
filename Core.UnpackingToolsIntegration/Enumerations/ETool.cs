04fb7d3d1f743603a6318398bbce7192a7e6ea44
Vitaly Andrev <v.andreev@systtech.ru>
2022-08-22 03:32:43 +0200

    chore: migrate to Kotiq's fork of WT Tools

--- Core.UnpackingToolsIntegration/Enumerations/ETool.cs
+++ Core.UnpackingToolsIntegration/Enumerations/ETool.cs
@@ -9,7 +9,7 @@ namespace Core.UnpackingToolsIntegration.Enumerations
         private const string _unpacker = _unpack + "er";
         private const string _exe = ECharacterString.Period + EFileExtension.Exe;
 
-        public const string BlkUnpacker = "blk" + _unpack + _exe;
+        public const string BlkUnpacker = "blk" + _unpack + "_ng" + _exe;
         public const string ClogUnpacker = "clog" + _unpack + _exe;
         public const string DdsxUnpacker = "ddsx" + _unpack + _exe;
         public const string DxpUnpacker = "dxp" + _unpack + _exe;
